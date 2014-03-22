5bd45e502a1d863e3dbf12e3fbc18d760595f606
LEDfan <tobia@ledfan.be>
2014-03-22 09:49:59 +0100

    Cleanup /lib/och/push

--- lib/och/push/delete.php
+++ lib/och/push/delete.php
@@ -6,28 +6,25 @@ use \OCA\Chat\OCH\ChatAPI;
 use \OCA\Chat\OCH\Db\PushMessage;
 use \OCA\Chat\OCH\Db\PushMessageMapper;
 use \OCA\Chat\Core\API;
-
 use \OCP\AppFramework\Http\JSONResponse;
-
 
 class Delete extends ChatAPI{
 
-	public function __construct(API $api){
-		parent::__construct($api);
-	}
+    public function __construct(API $api){
+        parent::__construct($api);
+    }
 
-	public function setRequestData(array $requestData){
-		$this->requestData = $requestData;
-	}
+    public function setRequestData(array $requestData){
+        $this->requestData = $requestData;
+    }
 
-	public function execute(){
+    public function execute(){
         foreach($this->requestData['ids'] as $id){                
             $pushMessage = new PushMessage();
-	        $pushMessage->setId($id);
+            $pushMessage->setId($id);
             $mapper = new PushMessageMapper($this->api);
             $mapper->delete($pushMessage);
         }
         return new JSONResponse(array('status' => 'success'));
-	}	
-
+    }	
 }
--- lib/och/push/get.php
+++ lib/och/push/get.php
@@ -4,44 +4,35 @@ namespace OCA\Chat\OCH\Push;
 
 use \OCA\Chat\Core\API;
 use \OCA\Chat\OCH\ChatAPI;
-use \OCA\Chat\OCH\Commands\Command;
-use \OCA\Chat\OCH\Exceptions\RequestDataInvalid;
 use \OCA\Chat\OCH\Db\PushMessageMapper;
-use \OCA\Chat\OCH\Db\PushMessage;
 use \OCA\Chat\Db\DoesNotExistException;
-
 use \OCP\AppFramework\Http\JSONResponse;
-
 
 class Get extends ChatAPI{
 
-	public function __construct(API $api){
-		parent::__construct($api);
-	}
+    public function __construct(API $api){
+        parent::__construct($api);
+    }
 
-	public function setRequestData(array $requestData){
-		$this->requestData = $requestData;
-	}
+    public function setRequestData(array $requestData){
+        $this->requestData = $requestData;
+    }
 
-	public function execute(){
-		session_write_close();
-     	try {
+    public function execute(){
+        session_write_close();
+        try {
             $mapper = new PushMessageMapper($this->api); // inject API class for db access
             $this->pushMessages = $mapper->findBysSessionId($this->requestData['session_id']);  
-            
         } catch(DoesNotExistException $e){
             sleep(1);
             $this->execute();
         }
-        
-        
+    
         $commands = array();
         foreach($this->pushMessages as $pushMessage){
-        	$command = json_decode($pushMessage->getCommand(), true);
+            $command = json_decode($pushMessage->getCommand(), true);
             $commands[$pushMessage->getId()] = $command;
         }
         return new JSONResponse(array('push_msgs' => $commands));
-
-	}	
-
+    }	
 }

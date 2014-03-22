c7288a112cdc844916f4a160e01d75a569baf63e
LEDfan <tobia@ledfan.be>
2014-03-22 09:31:01 +0100

    Cleanup di/

--- dependencyinjection/dicontainer.php
+++ dependencyinjection/dicontainer.php
@@ -37,134 +37,122 @@ class DIContainer extends SimpleContainer implements IAppContainer{
         $this->registerParameter('ServerContainer', \OC::$server);
 
         $this['API'] = $this->share(function($c){
-                return new API($c['AppName']);
+            return new API($c['AppName']);
         });
 
         /**
          * Http
          */
         $this['Request'] = $this->share(function($c) {
-                /** @var $c SimpleContainer */
-                /** @var $server IServerContainer */
-                $server = $c->query('ServerContainer');
-                $server->registerParameter('urlParams', $c['urlParams']);
-                return $server->getRequest();
+            /** @var $c SimpleContainer */
+            /** @var $server IServerContainer */
+            $server = $c->query('ServerContainer');
+            $server->registerParameter('urlParams', $c['urlParams']);
+            return $server->getRequest();
         });
 
         $this['Protocol'] = $this->share(function($c){
-                if(isset($_SERVER['SERVER_PROTOCOL'])) {
-                        return new Http($_SERVER, $_SERVER['SERVER_PROTOCOL']);
-                } else {
-                        return new Http($_SERVER);
-                }
+            if(isset($_SERVER['SERVER_PROTOCOL'])) {
+                    return new Http($_SERVER, $_SERVER['SERVER_PROTOCOL']);
+            } else {
+                    return new Http($_SERVER);
+            }
         });
 
         $this['Dispatcher'] = $this->share(function($c) {
-                return new Dispatcher($c['Protocol'], $c['MiddlewareDispatcher']);
+            return new Dispatcher($c['Protocol'], $c['MiddlewareDispatcher']);
         });
-
 
         /**
          * Middleware
          */
         $app = $this;
         $this['SecurityMiddleware'] = $this->share(function($c) use ($app){
-                return new SecurityMiddleware($app, $c['Request']);
+            return new SecurityMiddleware($app, $c['Request']);
         });
 
         $middleWares = $this->middleWares;
         $this['MiddlewareDispatcher'] = $this->share(function($c) use ($middleWares) {
-                $dispatcher = new MiddlewareDispatcher();
-                $dispatcher->registerMiddleware($c['SecurityMiddleware']);
+            $dispatcher = new MiddlewareDispatcher();
+            $dispatcher->registerMiddleware($c['SecurityMiddleware']);
 
-                foreach($middleWares as $middleWare) {
-                        $dispatcher->registerMiddleware($middleWare);
-                }
+            foreach($middleWares as $middleWare) {
+                    $dispatcher->registerMiddleware($middleWare);
+            }
 
-                return $dispatcher;
+            return $dispatcher;
         });
-
 
         /**
          * Utilities
          */
         $this['TimeFactory'] = $this->share(function($c){
-                return new TimeFactory();
+            return new TimeFactory();
         });
 
         $this['this'] = $this;
 
         $this['AppController'] = $this->share(function($c){
-        //   throw new \Exception("PAgecontroller callee");
-        return new AppController($c['this'], $c['Request']);
+            return new AppController($c['this'], $c['Request']);
         });
 
         $this['ApiController'] = $this->share(function($c){
-        //   throw new \Exception("PAgecontroller callee");
-        return new ApiController($c['this'], $c['Request']);
+            return new ApiController($c['this'], $c['Request']);
         });
         
         $this['AppApi'] = $this->share(function($c){
-        	return new AppApi($c['this']);
+            return new AppApi($c['this']);
         });
-
-        
-        
     }
-
-
 
     /**
      * @return IApi
      */
-    public function getCoreApi()
-    {
-          // throw new \Exception('hoooooooooooooooooooooooooooi');
-            return $this->query('API');
+    public function getCoreApi(){
+        return $this->query('API');
     }
     
 
     /**
      * @return \OCP\IServerContainer
      */
-    function getServer()
-    {
-            return $this->query('ServerContainer');
+    function getServer(){
+        return $this->query('ServerContainer');
     }
 
     /**
      * @param Middleware $middleWare
      * @return boolean
      */
-    function registerMiddleWare(Middleware $middleWare) {
-            array_push($this->middleWares, $middleWare);
+    function registerMiddleWare(Middleware $middleWare){
+        array_push($this->middleWares, $middleWare);
     }
 
     /**
      * used to return the appname of the set application
      * @return string the name of your application
      */
-    function getAppName() {
-            return $this->query('AppName');
+    function getAppName(){
+        return $this->query('AppName');
     }
 
     /**
      * @return boolean
      */
-    public function isLoggedIn() {
-            return \OC_User::isLoggedIn();
+    public function isLoggedIn(){
+        return \OC_User::isLoggedIn();
     }
 
     /**
      * @return boolean
      */
-    function isAdminUser() {
-            $uid = $this->getUserId();
-            return \OC_User::isAdminUser($uid);
+    function isAdminUser(){
+        $uid = $this->getUserId();
+        return \OC_User::isAdminUser($uid);
     }
 
-    private function getUserId() {
-            return \OC::$session->get('user_id');
+    private function getUserId(){
+        return \OC::$session->get('user_id');
     }
 
     /**
@@ -173,23 +161,23 @@ class DIContainer extends SimpleContainer implements IAppContainer{
      * @return mixed
      */
     function log($message, $level) {
-            switch($level){
-                    case 'debug':
-                            $level = \OCP\Util::DEBUG;
-                            break;
-                    case 'info':
-                            $level = \OCP\Util::INFO;
-                            break;
-                    case 'warn':
-                            $level = \OCP\Util::WARN;
-                            break;
-                    case 'fatal':
-                            $level = \OCP\Util::FATAL;
-                            break;
-                    default:
-                            $level = \OCP\Util::ERROR;
-                            break;
-            }
-            \OCP\Util::writeLog($this->getAppName(), $message, $level);
+        switch($level){
+            case 'debug':
+                $level = \OCP\Util::DEBUG;
+                break;
+            case 'info':
+                $level = \OCP\Util::INFO;
+                break;
+            case 'warn':
+                $level = \OCP\Util::WARN;
+                break;
+            case 'fatal':
+                $level = \OCP\Util::FATAL;
+                break;
+            default:
+                $level = \OCP\Util::ERROR;
+                break;
+        }
+        \OCP\Util::writeLog($this->getAppName(), $message, $level);
     }
 }

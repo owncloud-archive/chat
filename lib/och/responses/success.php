6b723c61212738822e9b0fc241b11124778937c7
LEDfan <tobia@ledfan.be>
2014-03-22 09:50:35 +0100

    Cleanup /lib/och/responses

--- lib/och/responses/success.php
+++ lib/och/responses/success.php
@@ -5,12 +5,11 @@ use \OCP\AppFramework\Http\JSONResponse;
 
 class Success extends JSONResponse{
 
-	public function __construct($requestType, $action, $data=array()){
-            $data["status"] = "success";
-            parent::__construct(array(
-			"type" => $requestType . "::" . $action . "::response" ,
-			"data" => $data
-		));
-	}
-
+    public function __construct($requestType, $action, $data=array()){
+        $data["status"] = "success";
+        parent::__construct(array(
+            "type" => $requestType . "::" . $action . "::response" ,
+            "data" => $data
+        ));
+    }
 }

4b6a27c7dc269c46737692157b87d0d11e11b0f4
LEDfan <tobia@ledfan.be>
2014-03-22 09:24:35 +0100

    Load correct CSS file

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,6 @@
 <?php 
 // Fist load all style sheets
-\OCP\Util::addStyle('chat', 'bootstrap-stripped');
+\OCP\Util::addStyle('chat', 'main');
 
 // Second load all dependencies
 \OCP\Util::addScript('chat', 'vendor/angular/angular.min');

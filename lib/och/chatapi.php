b378c1238d9a4751736dfce05a03b1b5d7eb0197
LEDfan <tobia@ledfan.be>
2014-03-22 09:50:58 +0100

    Cleanup /lib/och/chatapi

--- lib/och/chatapi.php
+++ lib/och/chatapi.php
@@ -6,22 +6,20 @@ use \OCA\Chat\Core\API;
 /*
  * API Base Class
  */
-
 abstract class ChatAPI {
 
-	public $api;
-	protected $requestData;
-	
-	public function __construct(API $api){
-		$this->api = $api;
-	}
-	
-	abstract function setRequestData(array $requestData);
+    public $api;
+    protected $requestData;
 
-	public function getRequestData(){
-		return $this->requestData;
-	}
+    public function __construct(API $api){
+        $this->api = $api;
+    }
 
-	abstract public function execute();	
+    abstract function setRequestData(array $requestData);
 
+    public function getRequestData(){
+        return $this->requestData;
+    }
+
+    abstract public function execute();	
 }

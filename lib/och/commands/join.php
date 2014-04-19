fe449fe09e073b1b1e247cb3851f5c1277337a00
LEDfan <tobia@ledfan.be>
2014-04-19 16:14:20 +0200

    On join add user to initconvs

--- lib/och/commands/join.php
+++ lib/och/commands/join.php
@@ -9,51 +9,59 @@ use \OCA\Chat\OCH\Db\UserMapper;
 use \OCA\Chat\OCH\Db\PushMessage;
 use \OCA\Chat\OCH\Db\PushMessageMapper;
 use OCA\Chat\OCH\Exceptions\RequestDataInvalid;
+use OCA\Chat\OCH\Db\InitConv;
 
 class Join extends ChatAPI {
-	
-    public function setRequestData(array $requestData){
-    	if(empty($requestData['conv_id'])){
-    		throw new RequestDataInvalid("CONV-ID-MUST-BE-PROVIDED");
-    	}
-        $this->requestData = $requestData;
-    }		
 
-    public function execute(){
-        $user = new User();
-        $user->setConversationId($this->requestData['conv_id']);
-        $user->setUser($this->requestData['user']['backends']['och']['value']);
-        $user->setSessionId($this->requestData['session_id']);
-        $userMapper = $this->app['UserMapper'];
-        $userMapper->insert($user);
+	public function setRequestData(array $requestData){
+		if(empty($requestData['conv_id'])){
+			throw new RequestDataInvalid("CONV-ID-MUST-BE-PROVIDED");
+		}
+		$this->requestData = $requestData;
+	}
 
-        return true;
+	public function execute(){
+		$user = new User();
+		$user->setConversationId($this->requestData['conv_id']);
+		$user->setUser($this->requestData['user']['backends']['och']['value']);
+		$user->setSessionId($this->requestData['session_id']);
+		$userMapper = $this->app['UserMapper'];
+		$userMapper->insert($user);
+
+		// mark this conv as a init conv => the conv is auto joined on refresh
+		$initConv = new InitConv();
+		$initConv->setConvId($this->requestData['conv_id']);
+		$initConv->setUser($this->requestData['user']['backends']['och']['value']);
+		$initConvMapper = $this->app['InitConvMapper'];
+		$initConvMapper->insertUnique($initConv);
+
+		return true;
 	/*
 	 * TODO
-        if (count($users) == 2){
-            // Send a push message to all users in this conversation to inform about a new user which joined
-            $command = json_encode(array(
-                "type" => 'joined',
-                "data" => array(
-                    "user" => $this->requestData['user'],
-                    "timestamp" => $this->requestData['timestamp'],
-                    "conv_id" => $this->requestData['conv_id']
-                )
-            ));
+		if (count($users) == 2){
+			// Send a push message to all users in this conversation to inform about a new user which joined
+			$command = json_encode(array(
+				"type" => 'joined',
+				"data" => array(
+					"user" => $this->requestData['user'],
+					"timestamp" => $this->requestData['timestamp'],
+					"conv_id" => $this->requestData['conv_id']
+				)
+			));
 
-            $sender = $this->requestData['user']; // copy the params('user') to a variable so it won't be called many times in a large conversation
-            $PushMessageMapper = $this->app['PushMessageMapper'];
-            foreach($users as $receiver){
-                if($receiver->getUser() !== $sender){
-                    $pushMessage = new PushMessage();
-                    $pushMessage->setSender($sender);
-                    $pushMessage->setReceiver($receiver->getUser());
-                    $pushMessage->setReceiverSessionId($receiver->getSessionId());
-                    $pushMessage->setCommand($command);
-                    $PushMessageMapper->insert($pushMessage);
-                }
-            }
-        } 
+			$sender = $this->requestData['user']; // copy the params('user') to a variable so it won't be called many times in a large conversation
+			$PushMessageMapper = $this->app['PushMessageMapper'];
+			foreach($users as $receiver){
+				if($receiver->getUser() !== $sender){
+					$pushMessage = new PushMessage();
+					$pushMessage->setSender($sender);
+					$pushMessage->setReceiver($receiver->getUser());
+					$pushMessage->setReceiverSessionId($receiver->getSessionId());
+					$pushMessage->setCommand($command);
+					$PushMessageMapper->insert($pushMessage);
+				}
+			}
+		}
 	*/
-    }	
+	}
 }
--- lib/och/db/initconvmapper.php
+++ lib/och/db/initconvmapper.php
@@ -2,6 +2,7 @@
 
 namespace OCA\Chat\OCH\Db;
 
+use OCA\Chat\Db\Entity;
 use OCA\Chat\Db\Mapper;
 
 class InitConvMapper extends Mapper{
@@ -12,11 +13,32 @@ class InitConvMapper extends Mapper{
 
 	/**
 	 * Deletes an entity from the table
+	 *
 	 * @param Entity $entity the entity that should be deleted
 	 */
-	public function deleteByConvAndUser(Entity $entity){
+	public function deleteByConvAndUser(Entity $entity) {
 		$sql = 'DELETE FROM `' . $this->tableName . '` WHERE `conv_id` = ? AND `user` = ?';
-		$this->execute($sql, array($entity->getConvId(), $entity->getUser()));
+		$this->execute($sql, array(
+				$entity->getConvId(),
+				$entity->getUser()
+		));
+	}
+
+	public function insertUnique(Entity $entity) {
+		$sql = 'INSERT INTO `' . $this->tableName . '` (conv_id, user)
+			SELECT * FROM (SELECT ?,?) AS tmp
+			WHERE NOT EXISTS (
+			SELECT conv_id, `user` FROM `' . $this->tableName .'` WHERE conv_id = ? AND `user` = ?
+			) LIMIT 1';
+
+		$this->execute($sql, array(
+			$entity->getConvId(),
+			$entity->getUser(),
+			$entity->getConvId(),
+			$entity->getUser(),
+		));
+
 	}
 
 }
+

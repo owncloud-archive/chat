8873309f3ae6f86fb0965ac7efdb76d7af170f1d
LEDfan <tobia@ledfan.be>
2014-05-25 09:59:22 +0200

    Solve issue when fetching chat messages and when in multiple convs

--- lib/och/db/messagemapper.php
+++ lib/och/db/messagemapper.php
@@ -7,12 +7,12 @@ use OCA\Chat\Db\Mapper;
 class MessageMapper extends Mapper{
 
 	public function __construct(API $api) {
-	parent::__construct($api, 'chat_och_messages'); // tablename is news_feeds
+		parent::__construct($api, 'chat_och_messages'); // tablename is news_feeds
 	}
  
 	public function getMessagesByConvId($convId, $user){
-		$sql = 'SELECT * FROM chat_och_messages WHERE `convid` = ? AND `timestamp` > (SELECT joined FROM chat_och_users_in_conversation WHERE `user` = ? )';
-		return $this->findEntities($sql, array($convId, $user));
+		$sql = 'SELECT * FROM  *PREFIX*chat_och_messages WHERE `convid` = ? AND `timestamp` > (SELECT joined FROM *PREFIX*chat_och_users_in_conversation  WHERE `user` = ? AND `conversation_id` = ?)';
+		return $this->findEntities($sql, array($convId, $user, $convId));
 	}
 
 }

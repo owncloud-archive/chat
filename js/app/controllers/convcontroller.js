80be3c1169d49b8301381e2ad5f1290b123719be
LEDfan <tobia@ledfan.be>
2014-07-16 15:25:13 +0200

    Fix issues with avatars in conv-list + auto-expand refs #68

--- js/app/controllers/convcontroller.js
+++ js/app/controllers/convcontroller.js
@@ -471,13 +471,49 @@ Chat.angular.controller('ConvController', ['$scope', '$filter', function($scope,
 		restrict: 'A',
 		link: function ($scope, element, attrs) {
 			var text  = '';
-			users = JSON.parse(attrs.users);
+			if(typeof attrs.users === 'string' ){
+				users = JSON.parse(attrs.users);
+			} else {
+				users = attrs.users;
+			}
 			angular.forEach(users, function(user, key){
 				if(user.id !== Chat.scope.active.user.id){
 					text += user.displayname + ' ';
 				}
 			});
 			element.text(text);
+		}
+	};
+}).directive('autoHeight', function () {
+	return {
+		restrict: 'A',
+		link: function ($scope, element, attrs) {
+			if(attrs.convId === $scope.active.conv){
+				attrs.itemCount++;
+			} else {
+				element.css('height', attrs.minHeight + 'px');
+			}
+			var height = attrs.itemCount * attrs.itemHeight;
+			if(height < attrs.minHeight){
+				element.css('height', attrs.minHeight + 'px');
+			} else {
+				element.css('height', height + 'px');
+			}
+			$scope.$watch('active.conv', function(){
+				if(attrs.convId === $scope.active.conv){
+					var height = attrs.itemCount * attrs.itemHeight;
+					if(attrs.itemCount > 1){
+						height = height + 30;
+					}
+					if(height < attrs.minHeight){
+						element.css('height', attrs.minHeight + 'px');
+					} else {
+						element.css('height', height + 'px');
+					}
+				} else {
+					element.css('height', attrs.minHeight + 'px');
+				}
+			});
 		}
 	};
 });
---
+++ templates/part.avatar.php
@@ -0,0 +1,71 @@
+<div ng-if="conv.id !== $parent.active.conv || ( conv.id === $parent.active.conv && conv.users.length === 2)" class="conv-list-item-avatar">
+	<div ng-if="conv.users.length === 2" class="avatar-list-container" >
+		<div
+			tipsy
+			title="{{ user.displayname }}"
+			ng-if="key < 4"
+			ng-repeat="(key, user) in conv.users | userFilter"
+			class="avatar-list-avatar-big"
+			avatar
+			data-size="40"
+			data-id="{{ user.id }}"
+			data-displayname="{{ user.displayname }}"
+			data-addressbook-backend="{{ user.address_book_backend }}"
+			data-addressbook-id="{{ user.address_book_id  }}"
+			>
+		</div>
+	</div>
+	<div ng-if="conv.users.length > 2" class="avatar-list-container" >
+		<div
+			tipsy
+			title="{{ user.displayname }}"
+			ng-if="key < 4"
+			ng-repeat="(key, user) in conv.users | userFilter"
+			class="avatar-list-avatar"
+			avatar
+			data-size="20"
+			data-id="{{ user.id }}"
+			data-displayname="{{ user.displayname }}"
+			data-addressbook-backend="{{ user.address_book_backend }}"
+			data-addressbook-id="{{ user.address_book_id  }}"
+			>
+		</div>
+	</div>
+	<span displayname data-users="{{ conv.users }}" class="left avatar-list-displayname" ng-class="{bold : conv.new_msg === true}" >
+	</span>
+</div>
+<div
+	class="conv-list-item-avatar"
+	ng-if="conv.id === $parent.active.conv && conv.users.length > 2"
+	>
+	<ul>
+		<li
+			ng-repeat="(key, user) in conv.users | userFilter"
+			class="avatar-list-expanded-item"
+			>
+			<div
+				tipsy
+				title="{{ user.displayname }}"
+				class="avatar-list-avatar"
+				avatar
+				data-size="20"
+				data-id="{{ user.id }}"
+				data-displayname="{{ user.displayname }}"
+				data-addressbook-backend="{{ user.address_book_backend }}"
+				data-addressbook-id="{{ user.address_book_id  }}"
+				>
+			</div>
+			{{ user.displayname }}
+		</li>
+		<li
+			class="avatar-list-expanded-item invite-button"
+			ng-click="$parent.view.toggle('invite');$parent.view.toggle('chat');"
+			>
+			<div
+				class="icon-add icon-20 avatar-list-avatar">
+				&nbsp;
+			</div>
+			Add Person
+		</li>
+	</ul>
+</div>

3886eece456bf1b26b70a5901842c4a116521936
LEDfan <tobia@ledfan.be>
2014-07-21 10:28:51 +0200

    Update clean_db.sql

--- tests/clean_db.sql
+++ tests/clean_db.sql
@@ -1,6 +1,5 @@
-DELETE FROM owncloud.chat_och_conversations;
-DELETE FROM owncloud.chat_och_messages;
-DELETE FROM owncloud.chat_och_push_messages;
-DELETE FROM owncloud.chat_och_users_in_conversation;
-DELETE FROM owncloud.chat_och_users_online;
-DELETE FROM owncloud.chat_och_init_convs;
+DELETE FROM owncloud.oc_chat_och_conversations;
+DELETE FROM owncloud.oc_chat_och_messages;
+DELETE FROM owncloud.oc_chat_och_push_messages;
+DELETE FROM owncloud.oc_chat_och_users_in_conversation;
+DELETE FROM owncloud.oc_chat_och_users_online;

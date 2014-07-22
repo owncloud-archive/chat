e3ccfe4deb4b099704acd867f5028c7b1eb0e0ef
LEDfan <tobia@ledfan.be>
2014-07-22 12:03:24 +0200

    Fix issue by initing och after document.ready

--- js/och/chat.och.util.js
+++ js/och/chat.och.util.js
@@ -1,36 +1,38 @@
 Chat.och.util = {
 	init : function(){
-		Chat.och.sessionId = Chat.scope.initvar.sessionId;
-		Chat.och.api.util.longPoll();
-		// Now join and add all the existing convs
-		angular.forEach(Chat.scope.initConvs.och, function(conv){
-			var contacts = [];
-			angular.forEach(conv.users, function(user){
-				contacts.push(Chat.scope.contactsObj[user]);
-			});
-			Chat.app.view.addConv(conv.id, contacts, Chat.scope.backends.och, [], conv.archived);
-			Chat.och.api.command.join(conv.id, function(){});
-			var cachedMsgs = Cache.get(conv.id);
-			if(cachedMsgs !== undefined){
-				cachedMsgs = cachedMsgs.msgs;
-				for (var i = 0; i < cachedMsgs.length; i++) {
-					var msg = cachedMsgs[i];
-					Chat.app.view.addChatMsg(conv.id, msg.user, msg.msg, msg.timestamp, Chat.scope.backends.och, true);
-					if(i === cachedMsgs.length -1){
-						var timestampLatestMsg = msg.timestamp;
-					}
-				}
-			} else {
-				var timestampLatestMsg = 0;
-			}
-			Chat.och.api.command.getMessages(conv.id, timestampLatestMsg, function(data){
-				data.data.messages.forEach(function(msg){
-					Chat.app.view.addChatMsg(conv.id, Chat.scope.contactsObj[msg.user], msg.msg, msg.timestamp, Chat.scope.backends.och, true);
+		$(function(){
+			Chat.och.sessionId = Chat.scope.initvar.sessionId;
+			Chat.och.api.util.longPoll();
+			// Now join and add all the existing convs
+			angular.forEach(Chat.scope.initConvs.och, function(conv){
+				var contacts = [];
+				angular.forEach(conv.users, function(user){
+					contacts.push(Chat.scope.contactsObj[user]);
 				});
-			});
+				Chat.app.view.addConv(conv.id, contacts, Chat.scope.backends.och, [], conv.archived);
+				Chat.och.api.command.join(conv.id, function(){});
+				var cachedMsgs = Cache.get(conv.id);
+				if(cachedMsgs !== undefined){
+					cachedMsgs = cachedMsgs.msgs;
+					for (var i = 0; i < cachedMsgs.length; i++) {
+						var msg = cachedMsgs[i];
+						Chat.app.view.addChatMsg(conv.id, msg.user, msg.msg, msg.timestamp, Chat.scope.backends.och, true);
+						if(i === cachedMsgs.length -1){
+							var timestampLatestMsg = msg.timestamp;
+						}
+					}
+				} else {
+					var timestampLatestMsg = 0;
+				}
+				Chat.och.api.command.getMessages(conv.id, timestampLatestMsg, function(data){
+					data.data.messages.forEach(function(msg){
+						Chat.app.view.addChatMsg(conv.id, Chat.scope.contactsObj[msg.user], msg.msg, msg.timestamp, Chat.scope.backends.och, true);
+					});
+				});
 
+			});
+			setInterval(Chat.och.util.updateOnlineStatus, 60000);
 		});
-		setInterval(Chat.och.util.updateOnlineStatus, 60000);
 	},
 	quit : function(){
 		Chat.och.api.command.offline();

60be1e0b686381e50523d2f3f1bfc81c398882f9
LEDfan <tobia@ledfan.be>
2014-07-30 13:22:06 +0200

    WIP

--- lib/och/push/get.php
+++ lib/och/push/get.php
@@ -7,9 +7,13 @@ use \OCA\Chat\OCH\ChatAPI;
 use \OCA\Chat\OCH\Db\PushMessageMapper;
 use \OCA\Chat\Db\DoesNotExistException;
 use \OCP\AppFramework\Http\JSONResponse;
+use \OCA\Chat\OCH\Responses\Error;
 
 class Get extends ChatAPI{
 
+	private $cycles = 0;
+
+	private $state;
 	public function setRequestData(array $requestData){
 		$this->requestData = $requestData;
 	}
@@ -17,22 +21,34 @@ class Get extends ChatAPI{
 	public function execute(){
 		session_write_close();
 		try {
-		$mapper = $this->app['PushMessageMapper']; // inject API class for db access
-			$this->pushMessages = $mapper->findBysSessionId($this->requestData['session_id']);
+			if($this->cycles > 5){
+				$this->state = 'TIME-EXCEEDED';
+			} else {
+				$mapper = $this->app['PushMessageMapper']; // inject API class for db access
+				$this->pushMessages = $mapper->findBysSessionId($this->requestData['session_id']);
+
+				$this->state = 'PUSH';
+			}
 		} catch(DoesNotExistException $e){
 			sleep(1);
+			$this->cycles++;
 			$this->execute();
 		}
 
-		$commands = array();
-		foreach($this->pushMessages as $pushMessage){
-//			var_dump($pushMessage->getCommand());
-			$command = json_decode($pushMessage->getCommand());
-//			var_dump($command);
-			$commands[$pushMessage->getId()] = $command;
+//		return new JSONResponse(array('push_msgs' => $commands));
+//		return $this->response;
+//		var_dump($this->response);
+		if($this->state === 'TIME-EXCEEDED'){
+			// out of time
+			return new Error('push', 'get', 'TIME_EXCEEDED');
+		} else {
+			// Found push Messages
+			$commands = array();
+			foreach($this->pushMessages as $pushMessage){
+				$command = json_decode($pushMessage->getCommand());
+				$commands[$pushMessage->getId()] = $command;
+			}
+			return new JSONResponse(array('push_msgs' => $commands));
 		}
-
-//		die();
-		return new JSONResponse(array('push_msgs' => $commands));
 	}
 }

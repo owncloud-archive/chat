7994ade815a32df1960fb82ad2b5768a723d1c4c
LEDfan <tobia@ledfan.be>
2014-07-31 10:14:41 +0200

    Online update online state when greeting ref #80

--- lib/och/commands/greet.php
+++ lib/och/commands/greet.php
@@ -6,6 +6,7 @@ use \OCA\Chat\OCH\ChatAPI;
 use \OCA\Chat\Core\API;
 use \OCA\Chat\OCH\Db\UserOnline;
 use \OCA\Chat\OCH\Db\UserOnlineMapper;
+use \OCA\Chat\OCH\Db\PushMessage;
 
 class Greet extends ChatAPI {
 
@@ -27,6 +28,36 @@ class Greet extends ChatAPI {
 		$userOnline->setLastOnline($requestData['timestamp']);
 		$mapper = $this->app['UserOnlineMapper'];
 		$mapper->insert($userOnline);
+
+
+		// The user is now online
+		// check if it was online before -> if so do noting -> if not send a push msg
+		// send a push message to every user to inform this
+		$command = json_encode(array(
+			'type' => 'online',
+			'data' => array(
+				'user' => $this->requestData['user'],
+				'timestamp' => $this->requestData['timestamp'],
+			)
+		));
+
+		$userOnlineMapper = $this->app['UserOnlineMapper'];
+		$users = $userOnlineMapper->getAll();
+
+		$sender = $this->requestData['user']['backends']['och']['value'];
+		$pushMessageMapper = $this->app['PushMessageMapper'];
+
+		foreach($users as $user){
+			if($user->getUser() !== $sender){
+				$pushMessage = new PushMessage();
+				$pushMessage->setSender($sender);
+				$pushMessage->setReceiver($user->getUser());
+				$pushMessage->setReceiverSessionId($user->getSessionId());
+				$pushMessage->setCommand($command);
+				$pushMessageMapper->insert($pushMessage);
+			}
+		}
+
 
 		return array("session_id" => $sessionId);
 	}

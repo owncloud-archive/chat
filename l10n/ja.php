0f6eb8dff0de12f5f044423b34399af5c86906af
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-07 01:11:12 -0400

    [tx-robot] updated from transifex

--- l10n/ja.php
+++ l10n/ja.php
@@ -1,5 +1,12 @@
 <?php
 $TRANSLATIONS = array(
-"Chat" => "チャット"
+"Chat" => "チャット",
+"New Conversation" => "新しい会話",
+"Hide Archived Conversations" => "過去の会話を非表示にする",
+"Show Archived Conversations" => "過去の会話を表示する",
+"Add Person" => "人を追加",
+"Chat Message" => "チャットメッセージ",
+"Click on a contact to add them to the conversation" => "連絡先をクリックして会話に追加",
+"Click on a contact to start a conversation" => "連絡先をクリックして会話を開始"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";

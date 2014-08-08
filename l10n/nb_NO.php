c90a0877807a98a4dca855d5669123a579ad50fe
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-08 01:11:13 -0400

    [tx-robot] updated from transifex

---
+++ l10n/nb_NO.php
@@ -0,0 +1,12 @@
+<?php
+$TRANSLATIONS = array(
+"Chat" => "Prat",
+"New Conversation" => "Ny samtale",
+"Hide Archived Conversations" => "Skjul arkiverte samtaler",
+"Show Archived Conversations" => "Vis arkiverte samtaler",
+"Add Person" => "Legg til person",
+"Chat Message" => "Pratmelding",
+"Click on a contact to add them to the conversation" => "Klikk på en kontakt for å legge dem til i samtalen",
+"Click on a contact to start a conversation" => "Klikk på en kontakt for å starte en samtale"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

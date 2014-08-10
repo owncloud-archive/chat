c40a3525d1e51b158447fd7822e52492762df149
Tobia De Koninck <tobia@ledfan.be>
2014-08-10 15:01:40 +0200

    Invite container working

--- templates/part.invite.php
+++ templates/part.invite.php
@@ -1,27 +1,29 @@
 <div  ng-if="view.elements.invite" id="invite-container">
-	<ul>
-		<li class="invite-contact-search">
+<!--	<div id="invite-container-inner">-->
+		<div class="invite-contact-search">
 			<input class="filter-field" type="text" placeholder="<?php p($l->t('Search in users'));?>" ng-model="inviteSearch.displayname">
-		</li>
-		<li
-			class="invite-contact"
-			ng-repeat="contact in contacts | userFilter | filter:inviteSearch"
-			ng-click="invite(contact)"
-		>
-			<div
-				data-size="20"
-				data-parent="true"
-				data-id="{{ contact.id }}"
-				data-displayname="{{ contact.displayname }}"
-				data-addressbook-backend="{{ contact.address_book_backend }}"
-				data-addressbook-id="{{ contact.address_book_id  }}"
-				avatar
-				class="invite-contact-avatar"
-				>
-			</div>
-			<div class="invite-contact-name">
-				{{ contact.displayname }}
-			</div>
-		</li>
-	</ul>
+		</div>
+		<ul>
+			<li
+				class="invite-contact"
+				ng-repeat="contact in contacts | userFilter | filter:inviteSearch"
+				ng-click="invite(contact)"
+			>
+				<div
+					data-size="20"
+					data-parent="true"
+					data-id="{{ contact.id }}"
+					data-displayname="{{ contact.displayname }}"
+					data-addressbook-backend="{{ contact.address_book_backend }}"
+					data-addressbook-id="{{ contact.address_book_id  }}"
+					avatar
+					class="invite-contact-avatar"
+					>
+				</div>
+				<div class="invite-contact-name">
+					{{ contact.displayname }}
+				</div>
+			</li>
+		</ul>
+<!--	</div>-->
 </div>

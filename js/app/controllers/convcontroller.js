53152ee9df4a52a11a9f433e5c8fb1fab6ad35d0
Tobia De Koninck <tobia@ledfan.be>
2014-08-13 17:28:26 +0200

    Filter users from conversation

--- js/app/controllers/convcontroller.js
+++ js/app/controllers/convcontroller.js
@@ -660,5 +660,17 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 			}
 		}
 	};
-})
+}).filter('filterUsersInConv', function() {
+	return function(contacts) {
+		var result = [];
+		var users = Chat.scope.convs[Chat.scope.active.conv].users;
+		angular.forEach(contacts, function(contact){
+			if($.inArray(contact, users) === -1){
+				result.push(contact);
+			}
+		});
+
+		return result;
+	};
+});
 

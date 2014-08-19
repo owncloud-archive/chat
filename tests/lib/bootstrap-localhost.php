055901a75d136756636b58ba88e9ef33e326abe3
Tobia De Koninck <tobia@ledfan.be>
2014-08-19 07:42:36 +0200

    Use Pimple from 3rdparty

--- tests/lib/bootstrap-localhost.php
+++ tests/lib/bootstrap-localhost.php
@@ -9,7 +9,7 @@ class OC {
 	public static $server;
 }
 
-require_once("vendor/Pimple/Pimple.php");
+require_once(__DIR__ . "/../../../../3rdparty/Pimple/Pimple.php");
 // to execute without owncloud, we need to create our own classloader
 spl_autoload_register(function ($className){
 	if (strpos($className, 'OCA\\') === 0) {

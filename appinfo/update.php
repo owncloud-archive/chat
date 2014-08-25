ad3ce787cf1fd1a6eedea9c6cc17f423db14a2f4
LEDfan <tobia@ledfan.be>
2014-08-25 15:13:58 +0200

    Fix bug when upgrading the app

---
+++ appinfo/update.php
@@ -0,0 +1,11 @@
+<?php
+namespace OCA\Chat;
+
+use \OCA\Chat\App\Chat;
+
+// Reforce creating of backends
+$query = \OCP\DB::prepare('DELETE FROM `*PREFIX*chat_backends`');
+$query->execute(array());
+
+$chat = new Chat();
+$chat->registerBackend('ownCloud Handle', 'och', 'x-owncloud-handle' , 1);

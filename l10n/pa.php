e38e33b1be9fcf217fcb6d97106c9b4c197a1cdd
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-30 01:11:14 -0400

    [tx-robot] updated from transifex

---
+++ l10n/pa.php
@@ -0,0 +1,10 @@
+<?php
+$TRANSLATIONS = array(
+"Chat" => "ਗੱਲਬਾਤ",
+"Search in conversations" => "ਸੰਵਾਦ ਵਿੱਚ ਲਭੋ",
+"Add Person" => "ਵਿਅਕਤੀ ਨੂੰ ਪਾਉਣਾ",
+"Chat Message" => "ਗੱਲਬਾਤ ਸੁਨੇਹਾ",
+"Search in users" => "ਵਰਤੋਂਕਾਰਾਂ ਵਿੱਚ ਲਭੋ",
+"There are no other users on this ownCloud." => "ਇਸ ownCloud ਤੇ ਕੋਈ ਹੋਰ ਵਰਤੋਂਕਾਰ ਨਹੀਂ ਹੈ।"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

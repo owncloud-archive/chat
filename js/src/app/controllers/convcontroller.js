6e01ec5e98a8846fe1607317525bb8a95ebac3cc
LEDfan <tobia@ledfan.be>
2014-09-14 11:03:29 +0200

    Add JSdocs for ConvController

--- js/src/app/controllers/convcontroller.js
+++ js/src/app/controllers/convcontroller.js
@@ -5,23 +5,71 @@ add/**
  * See the COPYING file.
  */
 Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$interval', function($scope, $http, $filter, $interval) {
+	/**
+	 * @var {object} convs
+	 */
 	$scope.convs = {};
+
+	/**
+	 * Contacts stored as an array with contact objects
+	 * @var {array} contacts
+	 */
 	$scope.contacts = [];
+
+	/**
+	 * Contacts storead as an array with id's
+	 * @var {array} convs
+	 */
 	$scope.contactsList = [];
+
+	/**
+	 * @var {object} backends
+	 */
 	$scope.backends = [];
+
+	/**
+	 * @var {object} initConvs
+	 */
 	$scope.initConvs = {};
+
+	/**
+	 * @var {object} active
+	 */
 	$scope.active = {
+		/**
+		 * Stores the active backend
+		 * @var {object} backend
+		 */
 		backend : {},
+		/**
+		 * Stores the active conv id
+		 * @var {string} conv
+		 */
 		conv : {},
+		/**
+		 * @var {bool} window
+		 */
 		window : true,
-		//user :
 	};
-	// $scope.userToInvite // generated in main.php // this is the user to invite in the new conv panel
+	/**
+	 * @var {object} title
+	 */
 	$scope.title = {};
+	/**
+	 * @var {object} title
+	 */
 	$scope.title.title = "";
+	/**
+	 * @var {object} default
+	 */
 	$scope.title.default = "Chat - ownCloud";
+	/**
+	 * @var {array} new_msgs
+	 */
 	$scope.title.new_msgs = [];
-	$scope.debug = [];
+	/**
+	 * @var {object} fields
+	 */
 	$scope.fields = {
 		'chatMsg' : '',
 	};
@@ -45,7 +93,9 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 	}
 	$scope.initDone = true;
 
-
+	/**
+	 * Function called when the app is quit
+	 */
 	$scope.quit = function(){
 		for(var namespace in $scope.backends){
 			var backend = $scope.backends[namespace];
@@ -55,11 +105,15 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 		}
 	};
 
-	$scope.selectBackend = function(backend){
-		$scope.active.backend = backend;
-	};
-
+	/**
+	 * Object used to interact with the view
+	 * @var {object} view
+	 */
 	$scope.view = {
+		/**
+		 * Stores whether an element should be visible
+		 * @var {object} elements
+		 */
 		elements : {
 			"emptyMsg" : true,
 			"chat" : false,
@@ -68,6 +122,9 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 			"emojiContainer" : false,
 			"invite" : false,
 		},
+		/**
+		 * Called when an invite button is clicked
+		 */
 		inviteClick : function(){
 			$scope.view.toggle('invite');
 			// Focus search field
@@ -75,6 +132,9 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 				$('#invite-search-field').focus();
 			}, 1);
 		},
+		/**
+		 * Called when the popover button is clicked
+		 */
 		showEmojiPopover : function(){
 			var height = $("#chat-window-footer").height();
 			$scope.view.toggle('emojiContainer');
@@ -83,6 +143,12 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 			},1);
 			console.log(height);
 		},
+		/**
+		 * This will flag the element as visible in the view.elements array
+		 * @param {string} element the element which should be made visible (should be in the $scope.view.elements array )
+		 * @param {object} $event
+		 * @param {string} exception this function call will be ignored when this element is clicked
+		 */
 		show : function(element, $event, exception){
 			if($event !== undefined){
 				var classList = $event.target.classList;
@@ -95,6 +161,12 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 			}
 			$scope.view.elements[element] = true;
 		},
+		/**
+		 * This will flag the element as invisible in the view.elements array
+		 * @param {string} element the element which should be made invisible (should be in the $scope.view.elements array )
+		 * @param {object} $event
+		 * @param {string} exceptions this function call will be ignored when this element is clicked
+		 */
 		hide : function(element, $event, exceptions){
 			if($event !== undefined){
 				var classList = $event.target.classList;
@@ -109,17 +181,26 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 			}
 			$scope.view.elements[element] = false;
 		},
-		toggle : function(element, $event){
+		/**
+		 * This will toggle the visibility of the element in the view.elements array
+		 * @param {string} element the element which should be made invisible (should be in the $scope.view.elements array )
+		 */
+		toggle : function(element){
 			$scope.view.elements[element] = !$scope.view.elements[element];
-			if ($event !== undefined) {
-				if (typeof $event.stopPropagation === "function") {
-				$event.stopPropagation();
-				}
-			}
 		},
+		/**
+		 * This function will update the title of the page
+		 * @param {string} newTitle
+		 */
 		updateTitle : function(newTitle){
 			$scope.title = newTitle;
 		},
+		/**
+		 * This function will make an conv active
+		 * @param {string} convId
+		 * @param {object} $event
+		 * @param {string} exception - when this is provided the function call will be ignored when it was this param which was clicked
+		 */
 		makeActive : function(convId, $event, exception){
 			$scope.view.hide('emptyMsg');
 			$scope.view.show('chat', $event, exception);
@@ -138,9 +219,19 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 				}
 			});
 		},
+		/**
+		 * This will unActive all conversations
+		 */
 		unActive : function(){
 			$scope.active.conv = null;
 		},
+		/**
+		 * This will add an conv to the view
+		 * @param {string} convId
+		 * @param {array} users
+		 * @param {object} backend
+		 * @param {array} msgs
+		 */
 		addConv : function(convId, users, backend, msgs){
 			// generate conv name + higher order of contacts
 			var name  = '';
@@ -153,13 +244,9 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 				}
 			}
 			// end generate conv name
-
-
 			if($scope.convs[convId] === undefined) {
 				// get highest order
-
 				var order = $scope.getHighestOrder();
-
 				$scope.convs[convId] = {
 					id : convId,
 					users : users,
@@ -179,6 +266,18 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 				}
 			}
 		},
+		/**
+		 * This will add an chat msg to an existing conversation
+		 * This will call the notify() function when the message isn't send by the active user
+		 * This will call the notifyMsgInConv() function when it isn't the active conv
+		 * This will make the order of the conv the highest
+		 * @param {string} convId
+		 * @param {object} user
+		 * @param {string} msg
+		 * @param {int} timestamp
+		 * @param {object} backend
+		 * @param {bool} noNotify
+		 */
 		addChatMsg : function(convId, user, msg, timestamp, backend, noNotify){
 			if(user.id !== $scope.active.user.id){
 				$scope.notify(user.displayname);
@@ -256,35 +355,65 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 			$scope.convs[convId].raw_msgs.push({"msg" : msg, "timestamp" : timestamp, "user" : user});
 			$scope.convs[convId].order = $scope.getHighestOrder() +1;
 		},
+		/**
+		 * This will add an user to an conversation
+		 * @param {string} convId
+		 * @param {object} user
+		 */
 		addUserToConv : function(convId, user){
 			if($scope.convs[convId].users.indexOf(user) === -1){
 				$scope.convs[convId].users.push(user);
 			}
 		},
+		/**
+		 * This will focus the chat input field
+		 */
 		focusMsgInput : function(){
 			$('#chat-msg-input-field');
 		},
+		/**
+		 * This will replace the users in an existing conversation
+		 * @param {string} convId
+		 * @param {array} users - array with objects
+		 */
 		replaceUsers : function(convId, users){
 			$scope.convs[convId].users = users;
 		},
+		/**
+		 * This will bold the conversation name in the view
+		 * @param {string} convId
+		 */
 		notifyMsgInConv : function(convId){
 			$scope.convs[convId].new_msg = true;
 		},
+		/**
+		 * This will flag a contact in the $scope.contactsObj as online
+		 * @param {string} userId
+		 */
 		makeUserOnline : function(userId){
 			$scope.contactsObj[userId].online = true;
 		},
+		/**
+		 * This will flag a contact in the $scope.contactsObj as offline
+		 * @param {string} userId
+		 */
 		makeUserOffline : function(userId){
 			$scope.contactsObj[userId].online = false;
 		}
 	};
 
-
+	/**
+	 * This function is called when the user tries to send a chat msg
+	 * This will call the sendChatMsg function on the correct backend
+	 * This will add the chatMsg to the conv
+	 * This will empty the chat field
+	 * This will make the order of the contacts in the conv the highest
+	 */
 	$scope.sendChatMsg = function(){
 		if ($scope.fields.chatMsg !== ''){
 			var backend = $scope.convs[$scope.active.conv].backend.name;
 			$scope.view.addChatMsg($scope.active.conv, $scope.active.user, $scope.fields.chatMsg, Time.now(), backend);
 			Chat[backend].on.sendChatMsg($scope.active.conv, $scope.fields.chatMsg);
-			$scope.debug.push($scope.fields.chatMsg);
 			$scope.fields.chatMsg = '';
 			var order = $scope.getHighestOrderContacts();
 			setTimeout(function(){
@@ -302,6 +431,9 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 		}
 	};
 
+	/**
+	 * This function will make the first conversation in the conversation list active
+	 */
 	$scope.makeFirstConvActive = function(){
 		firstConv = $scope.getFirstConv();
 		if(firstConv === undefined){
@@ -313,6 +445,10 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 		}
 	};
 
+	/**
+	 * This function will return the first conversation in the conversation list
+	 * @returns {object|undefined}
+	 */
 	$scope.getFirstConv = function(){
 		for (firstConv in $scope.convs) break;
 		if (typeof firstConv !== 'undefined') {
@@ -322,6 +458,11 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 		}
 	}
 
+	/**
+	 * This function will invite the userToInvite for the $scope.active.conv
+	 * This will make the order of the conv the highest
+	 * @param {object} userToInvite
+	 */
 	$scope.invite = function(userToInvite){
 		var backend = $scope.convs[$scope.active.conv].backend.name;
 		if(userToInvite !== $scope.active.user && userToInvite !== ''){
@@ -337,16 +478,9 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 
 	};
 
-	function updateContacts(){
-		var url = OC.generateUrl('/apps/chat/contacts')
-		$http.get('/index.php/apps/chat/contacts?requesttoken=' + oc_requesttoken)
-			.success(function(data, status) {
-				$scope.contacts = data.contacts;
-				$scope.contactsObj = data.contactsObj;
-			});
-	};
-
-//	$interval(updateContacts, 10000);
+	/**
+	 * This interval will make the title of the page $scope.title.title every second
+	 */
 	$interval(function(){
 		if($scope.title.title === ''){
 			$('title').text($scope.title.default);
@@ -354,10 +488,20 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 			$('title').text($scope.title.title);
 		}
 	}, 1000);
+	/**
+	 * This interval will make the title of the page $scope.title.default every two second
+	 */
 	$interval(function(){
 		$('title').text($scope.title.default);
 	}, 2000);
 
+	/**
+	 * This will check if the window is active
+	 *  if so
+	 *   it will change the title of the page into 'New messages from {user}'
+	 *  otherwise
+	 *   it will empty the title
+	 */
 	$scope.$watchCollection('title.new_msgs', function(){
 		if($scope.active.window === false){
 			var title = 'New messages from ';
@@ -375,6 +519,11 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 		}
 	});
 
+	/**
+	 * This function will add the user to the $scope.title.new_msgs array
+	 * This way the user can be notified about a new msgs
+	 * @param {string} user
+	 */
 	$scope.notify = function(user){
 		if($scope.title.new_msgs.indexOf(user) == -1){
 			$scope.title.new_msgs.push(user);
@@ -391,6 +540,11 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 		$scope.active.window = false; 
 	};
 
+	/**
+	 * This function will add the emoji to the current position of the cursor in the chat msg input field
+	 * it will place a space before and after the emoji
+	 * @param name
+	 */
 	$scope.addEmoji = function(name){
 		var element = $("#chat-msg-input-field");
 		element.focus(); //ie
@@ -403,12 +557,19 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 
 	$scope.emojis = Chat.app.util.emojis;
 
+	/**
+	 * When there are new messages in the active conversation the chat window must be scroll to the bottom
+	 */
 	$scope.$watch('convs[active.conv].msgs', function(){
 		setTimeout(function(){
 			$('#chat-window-msgs').scrollTop($('#chat-window-msgs')[0].scrollHeight);
 		},250);
 	}, true);
 
+	/**
+	 * This function returns the order of an conversation increased with 1
+	 * @returns {int}
+	 */
 	$scope.getHighestOrder = function(){
 		var sortedConvs = $filter('orderObjectBy')($scope.convs, 'order');
 		if(sortedConvs[sortedConvs.length - 1] !== undefined){
@@ -418,6 +579,10 @@ Chat.angular.controller('ConvController', ['$scope', '$http', '$filter', '$inter
 		}
 	};
 
+	/**
+	 * This function returns the order of an contact increased with 1
+	 * @returns {int}
+	 */
 	$scope.getHighestOrderContacts = function(){
 		var sortedContacts = $filter('orderObjectBy')($scope.contactsObj, 'order');
 		return sortedContacts[sortedContacts.length - 1].order + 1;

27d7a02a7b09d1f2bc0b9e51123ddd256b6a0fcf
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-18 01:11:12 -0400

    [tx-robot] updated from transifex

--- l10n/el.php
+++ l10n/el.php
@@ -3,9 +3,9 @@ $TRANSLATIONS = array(
 "Chat" => "Συνομιλία",
 "Search in conversations" => "Αναζήτηση στις συζητήσεις",
 "Add Person" => "Προσθήκη Ατόμου",
-"Chat Message" => "Μύνημα Συνομιλίας",
+"Chat Message" => "Μήνυμα Συνομιλίας",
 "Search in users" => "Αναζήτηση χρηστών",
 "There are no other users on this ownCloud." => "Δεν υπάρχουν άλλοι χρήστες σε αυτό το ownCloud.",
-"In order to chat please create at least one user, it will appear on the left." => "Για να συνομιλήσετε, παρακαλώ δημιουργήστε τουλάχιστον ένα χρήστη, θα εμφανιστεί στα αριστερά."
+"In order to chat please create at least one user, it will appear on the left." => "Για να συνομιλήσετε παρακαλώ δημιουργήστε τουλάχιστον ένα χρήστη, θα εμφανιστεί στα αριστερά."
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

02147605b07b7983674a3dba0ed1238418fe1918
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-19 01:11:15 -0400

    [tx-robot] updated from transifex

--- l10n/sk_SK.php
+++ l10n/sk_SK.php
@@ -5,6 +5,7 @@ $TRANSLATIONS = array(
 "Add Person" => "Pridať osobu",
 "Chat Message" => "Správa",
 "Search in users" => "Vyhľadať v používateľoch",
-"There are no other users on this ownCloud." => "Na tomto ownCloude nie sú iní používatelia."
+"There are no other users on this ownCloud." => "Na tomto ownCloude nie sú iní používatelia.",
+"In order to chat please create at least one user, it will appear on the left." => "Na správne fungovanie chatu je potrebné mať vytvorené aspoň jedno používateľské meno, ktoré sa neskôr objaví na paneli vľavo."
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";

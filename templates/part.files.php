c24c982cb8b149dcd2c25ebeb048d0e042f7cc89
LEDfan <tobia@ledfan.be>
2014-10-06 12:04:50 +0200

    Small style fixes

--- templates/part.files.php
+++ templates/part.files.php
@@ -1,37 +1,44 @@
 <div ng-if="view.elements.files" class="files-no-hide" id="files-container">
-		<table>
-			<tr class="file-row">
-				<th class="file-owner file-element">
-					Owner
-				</th>
-				<th class="file-time file-element">
-					Time
-				</th>
-				<th class="file-path file-element">
-					Path
-				</th>
-				<th>
-					Preview
-				</th>
-			</tr>
-			<tr
-				class="file-row"
-				ng-repeat="file in convs[active.conv].files"
+	<p class="files-title"><?php p($l->t('Files attached to this conversation')); ?></p>
+	<table>
+		<tr>
+			<th class="file-preview">
+				&nbsp;
+			</th>
+			<th class="file-owner file-element">
+				Owner
+			</th>
+			<th class="file-time file-element">
+				Time
+			</th>
+			<th class="file-path file-element">
+				Path
+			</th>
+		</tr>
+		<tr
+			class="file-row"
+			ng-repeat="file in convs[active.conv].files"
+		>
+			<td class="file-preview">
+				<img src='/index.php/core/preview.png?file={{ file.path }}&x=36&y=36&forceIcon=1' >
+			</td>
+			<td class="file-owner file-element">
+				{{ file.user.displayname }}
+			</td>
+			<td lass="file-time file-element"
+				time
+				data-timestamp="{{ file.timestamp }}"
 			>
-				<td class="file-owner file-element">
-					{{ file.user.displayname }}
-				</td>
-				<td lass="file-time file-element"
-					time
-					data-timestamp="{{ file.timestamp }}"
-				>
-				</td>
-				<td class="file-path file-element ">
-					{{ file.path }}
-				</td>
-				<td>
-					<img src='/index.php/core/preview.png?file={{ file.path }}&x=36&y=36&forceIcon=1' >
-				</td>
-			</tr>
-		</table>
+			</td>
+			<td class="file-path file-element ">
+				{{ file.path }}
+			</td>
+		</tr>
+	</table>
+	<p
+		class="files-title"
+		ng-click="view.showFilePicker()"
+	>
+		<?php p($l->t('Attach more files')); ?>
+	</p>
 </div>

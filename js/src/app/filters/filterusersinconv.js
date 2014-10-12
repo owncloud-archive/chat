db2fb57c6b7105134eb9cb9a1f29e90edb3facfd
Tobia <tobia@ledfan.be>
2014-10-12 13:43:35 +0200

    Solve bug with sortings users by conv

--- js/src/app/filters/filterusersinconv.js
+++ js/src/app/filters/filterusersinconv.js
@@ -2,11 +2,17 @@ angular.module('chat').filter('filterUsersInConv', ['activeConv', 'convs', funct
 	return function(contacts) {
 		var result = [];
 		var users = convs.get(activeConv()).users;
-		angular.forEach(contacts, function(contact){
-			if($.inArray(contact, users) === -1){
-				result.push(contact);
-			}
-		});
+		var usersId = [];
+        for (var key in users){
+            var user = users[key];
+            usersId.push(user.id);
+        }
+        for (var key in contacts){
+            var contact = contacts[key];
+            if($.inArray(contact.id, usersId) === -1){
+                result.push(contact);
+            }
+        }
 		return result;
-	};
+    };
 }]);

789fb5a79058fa7126024021a93fa2a1a20a782b
Tobia <tobia@ledfan.be>
2014-10-12 13:12:25 +0200

    Readd check for contacts

---
+++ js/src/app/filters/count.js
@@ -0,0 +1,9 @@
+angular.module('chat').filter('count', function() {
+    return function(items) {
+        var count = 0;
+        for (var key in items){
+            count++;
+        }
+        return count;
+    };
+});S

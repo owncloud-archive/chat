e85fe6bec890e1d244c15d43aace88533c74b89a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-29 01:11:15 -0400

    [tx-robot] updated from transifex

--- l10n/eu.php
+++ l10n/eu.php
@@ -1,9 +1,15 @@
 <?php
 $TRANSLATIONS = array(
 "Chat" => "Berriketa",
+"{displayname} attached {path} to this conversation" => "{displayname} elkarrizketa honetara {path} atxikitu du",
+"{displayname} removed {path} from this conversation" => "{displayname} elkarrizketa honetatik {path} ezabatu du",
 "Search in conversations" => "Bilatu berriketetan",
 "Add Person" => "Gehitu pertsona",
+"View Attached files" => "Ikusi Erantsitako fitxategiak",
 "Chat Message" => "Berriketa mezua",
+"Files attached to this conversation" => "Elkarrizketa honetara atxikitutako fitxategiak",
+"Download " => "Deskargatu",
+"Attach more files" => "Gehitu fitxategi gehiago",
 "Search in users" => "Bilatu erabiltzaileetan",
 "There are no other users on this ownCloud." => "Ez dago beste erabiltzailerik ownCloud honetan.",
 "In order to chat please create at least one user, it will appear on the left." => "Berriketan aritzeko sortu gutxienez erabiltzaile bat, ezkerrean agertuko da."

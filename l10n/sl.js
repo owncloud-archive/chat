f4feaba9da5814f468d36c7a0bd3168281fe08cb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-11-08 01:11:11 -0500

    [tx-robot] updated from transifex

--- l10n/sl.js
+++ l10n/sl.js
@@ -12,6 +12,10 @@ OC.L10N.register(
     "Download " : "Prejmi",
     "Attach more files" : "Priloži več datotek",
     "Search in users" : "Poišči med uporabniki",
+    "There are no Chat Backends enabled." : "Ni omogočenega ozadnjega programa za klepetalice.",
+    "In order to chat please enable at least one Chat backend." : "Za klepet je treba najprej omogočiti vsaj en ozadnji program za klepetalnice.",
+    "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." : "Z ozadnjim programom 'OCH' je mogoče klepetati z drugimi uporabniki okolja ownCloud. Program ne zahteva posebnih nastavitev.",
+    "It can be enabled by running the following command in the root of your owncloud installation:" : "Program za klepet znotraj okolja ownCloud lahko omogočite z zagonom ukaza v korenu namestitve:",
     "There are no other users on this ownCloud." : "Ni drugih uporabnikov oblaka ownCloud.",
     "In order to chat please create at least one user, it will appear on the left." : "Za klepet je treba ustvariti vsaj enega uporabnika. Njegovo ime bo prikazano na levi."
 },

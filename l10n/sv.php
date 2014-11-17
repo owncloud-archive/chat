60b98b2eb66d2f109ccc94b07079354e3106aebd
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-11-17 01:11:11 -0500

    [tx-robot] updated from transifex

---
+++ l10n/sv.php
@@ -0,0 +1,21 @@
+<?php
+$TRANSLATIONS = array(
+"Chat" => "Chat",
+"{displayname} attached {path} to this conversation" => "{displayname} fäst fil {sökväg} till den här konversationen",
+"{displayname} removed {path} from this conversation" => "{displayname} tog bort {sökväg} från den här konversationen",
+"Search in conversations" => "Sök i dialog",
+"Add Person" => "Lägg till person",
+"View Attached files" => "Visa Bifogade filer",
+"Chat Message" => "Chat Meddelande",
+"Files attached to this conversation" => "Filer som bifogats i konversationen",
+"Download " => "Ladda ner",
+"Attach more files" => "Fler bifoga filer",
+"Search in users" => "Sök användare",
+"There are no Chat Backends enabled." => "Det finns inget Chat backend aktiverat.",
+"In order to chat please enable at least one Chat backend." => "För att chatta, vänligen aktivera minst en Chat backend.",
+"With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "Med den \"OCH\" backend kan du chatta med andra ownCloud användare. Det fungerar utan konfiguration.",
+"It can be enabled by running the following command in the root of your owncloud installation:" => "Den kan aktiveras genom att köra följande kommando i roten på din owncloud installation:",
+"There are no other users on this ownCloud." => "Det finns inga andra användare på denna ownCloud.",
+"In order to chat please create at least one user, it will appear on the left." => "För att chatta du skapa minst en användare, kommer den att visas på den vänstra."
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

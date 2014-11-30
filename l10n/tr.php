eb281a641952e4dabecc9474afb971b6d707fb64
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-11-30 01:11:17 -0500

    [tx-robot] updated from transifex

--- l10n/tr.php
+++ l10n/tr.php
@@ -14,6 +14,7 @@ $TRANSLATIONS = array(
 "There are no Chat Backends enabled." => "Etkin Sohbet Arka Ucu yok.",
 "In order to chat please enable at least one Chat backend." => "Sohbeti kullanabilmek için için lütfen en az bir sohbet arka ucu etkinleştirin.",
 "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "'OCH' arka ucu ile diğer ownCloud kullanıcıları ile sohbet edebilirsiniz. Yapılandırma olmadan çalışır.",
+"It can be enabled by running the following command in the root of your ownCloud installation:" => "ownCloud kurulum dizininde aşağıdaki komut çalıştırılarak etkin hale getirilebilir:",
 "There are no other users on this ownCloud." => "Bu ownCloud üzerinde başka kullanıcı yok.",
 "In order to chat please create at least one user, it will appear on the left." => "Sohbet edebilmek için lütfen en az bir kullanıcı oluşturun. Ardından sol bölümde görülecek."
 );

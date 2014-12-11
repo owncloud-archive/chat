e4a9b12c872f85a09b9fc404382f00f15564825b
LEDfan <tobia@ledfan.be>
2014-12-11 17:11:45 +0100

    Remove uneeded folders

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 # This file packages the ownCloud Chat app
 # This file is meant to run on the Jenkins server used by the Chat app. (https://occjen-ledfan.rhcloud.com)
 export HOME=/var/lib/openshift/547b566ee0b8cd470a000128/app-root/data/jobs/ownCloud-Chat/workspace
-files_to_delete=("node_modules" ".bowerrc" "bower.json" ".git*" "./.scrutinizer.yml" "./.travis.yml" "Gruntfile.js" "karma.conf.js" "Makefile" "package.json" "build.sh" "tests")
+files_to_delete=(".npm" ".node-gyp" "node_modules" ".bowerrc" "bower.json" ".git*" "./.scrutinizer.yml" "./.travis.yml" "Gruntfile.js" "karma.conf.js" "Makefile" "package.json" "build.sh" "tests")
 rm -rf ../chat
 mkdir ../chat
 
-#../../../node-v0.10.0-linux-x64/bin/node node_modules/grunt-cli/bin/grunt
 ../../../node-v0.10.33-linux-x64/bin/npm install
 ../../../node-v0.10.33-linux-x64/bin/node node_modules/grunt-cli/bin/grunt
 

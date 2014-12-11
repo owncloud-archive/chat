931d4c65cbece43037702a01e668b58b0b4c0230
LEDfan <tobia@ledfan.be>
2014-12-11 17:13:30 +0100

    Remove uneeded emojis

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # This file packages the ownCloud Chat app
 # This file is meant to run on the Jenkins server used by the Chat app. (https://occjen-ledfan.rhcloud.com)
 export HOME=/var/lib/openshift/547b566ee0b8cd470a000128/app-root/data/jobs/ownCloud-Chat/workspace
-files_to_delete=(".npm" ".node-gyp" "node_modules" ".bowerrc" "bower.json" ".git*" "./.scrutinizer.yml" "./.travis.yml" "Gruntfile.js" "karma.conf.js" "Makefile" "package.json" "build.sh" "tests")
+files_to_delete=("vendor/emojione/assets/svg" "vendor/emojione/assets/png" ".npm" ".node-gyp" "node_modules" ".bowerrc" "bower.json" ".git*" "./.scrutinizer.yml" "./.travis.yml" "Gruntfile.js" "karma.conf.js" "Makefile" "package.json" "build.sh" "tests")
 rm -rf ../chat
 mkdir ../chat
 
@@ -13,7 +13,7 @@ mkdir ../chat
 cp -r . ../chat
 
 for file in ${files_to_delete[@]}
-do
+dogit a
     rm -rf ../chat/$file
 done
 

06c2a697bfd9fdc74386ed5d01a6e958438883f1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-01 01:11:15 -0500

    [tx-robot] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Chat" : "Discussion",
     "{displayname} attached {path} to this conversation" : "{displayname} a attaché {path} à la conversation",
     "{displayname} removed {path} from this conversation" : "{displayname} a supprimé {path} de la conversation",
+    "Start a chat with ..." : "Démarrer une conversation avec ...",
     "Search in conversations" : "Rechercher dans les conversations",
     "Add Person" : "Ajouter une personne",
     "View Attached files" : "Voir les fichiers attachés",

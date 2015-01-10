95f3bc3a07c2787df284507eac42c196b5871737
LEDfan <tobia@ledfan.be>
2015-01-10 07:47:12 +0100

    Buttons height is 44px

--- js/src/app/directives/autoheight.js
+++ js/src/app/directives/autoheight.js
@@ -2,24 +2,13 @@ angular.module('chat').directive('autoHeight', [function () {
 	return {
 		restrict: 'A',
 		link: function ($scope, element, attrs) {
-			if(attrs.convId === $scope.$session.conv){
-				attrs.itemCount++;
-			} else {
-				element.css('height', attrs.minHeight + 'px');
-			}
-			var height = attrs.itemCount * attrs.itemHeight;
-			if(height < attrs.minHeight){
-				element.css('height', attrs.minHeight + 'px');
-			} else {
-				element.css('height', height + 'px');
-			}
-			//TODO
 			$scope.$watch('$session.conv', function(){
 				if(attrs.convId === $scope.$session.conv){
 					var height = attrs.itemCount * attrs.itemHeight;
 					if(attrs.itemCount > 1){
-						height = height + 30;
+						height = height + 88;
 					}
+
 					if(height < attrs.minHeight){
 						element.css('height', attrs.minHeight + 'px');
 					} else {

0b50d21191a7762fc9142d1b21dab45e179cdb72
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-17 01:11:13 -0500

    [tx-robot] updated from transifex

---
+++ l10n/ko.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "chat",
+    {
+    "Chat" : "대화",
+    "{displayname} attached {path} to this conversation" : "{displayname} 님이 이 대화에 {path}을(를) 첨부함",
+    "{displayname} removed {path} from this conversation" : "{displayname} 님이 이 대화에서 첨부 {path}을(를) 삭제함",
+    "Start a chat with ..." : "대화 시작...",
+    "Search in conversations" : "대화 목록에서 찾기",
+    "Add Person" : "사람 추가",
+    "View Attached files" : "첨부 파일 보기",
+    "Chat Message" : "대화 메시지",
+    "Files attached to this conversation" : "이 대화에 첨부된 파일",
+    "Download " : "다운로드 ",
+    "Attach more files" : "더 많은 파일 첨부",
+    "Search in users" : "사용자에서 찾기",
+    "There are no Chat Backends enabled." : "대화 백엔드가 활성화되지 않았습니다.",
+    "In order to chat please enable at least one Chat backend." : "대화 기능을 사용하려면 하나 이상의 대화 백엔드가 필요합니다.",
+    "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." : "'OCH' 백엔드를 사용하면 다른 ownCloud 사용자와 대화할 수 있으며, 설정하지 않아도 됩니다.",
+    "It can be enabled by running the following command in the root of your ownCloud installation:" : "ownCloud 설치 최상위 경로에서 다음 명령을 실행하여 활성화할 수 있습니다:",
+    "There are no other users on this ownCloud." : "이 ownCloud를 사용하는 다른 사용자가 없습니다.",
+    "In order to chat please create at least one user, it will appear on the left." : "대화를 시작하려면 최소한 한 명의 사용자가 필요하며, 왼쪽에 나타날 것입니다."
+},
+"nplurals=1; plural=0;");

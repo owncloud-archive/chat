53e02be41be1ca7826f4d222ea12625c77a928e8
LEDfan <tobia@ledfan.be>
2015-02-09 12:03:36 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 export HOME=/var/lib/openshift/547b566ee0b8cd470a000128/app-root/data/jobs/ownCloud-Chat/workspace
 files_to_delete=("vendor/emojione/assets/svg" "vendor/emojione/assets/png" ".npm" ".node-gyp" "node_modules" ".bowerrc" "bower.json" ".git*" "./.scrutinizer.yml" "./.travis.yml" "Gruntfile.js" "karma.conf.js" "Makefile" "package.json" "build.sh" "tests")
 rm -rf ../chat
+rm -rf .npm
+rm -rf node_modules
 mkdir ../chat
 
 ../../../node-v0.10.33-linux-x64/bin/npm install

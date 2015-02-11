85f0c97336db40ee89bf66f6bc80e6767fe71cb0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-11 01:11:13 -0500

    [tx-robot] updated from transifex

--- l10n/fr.php
+++ l10n/fr.php
@@ -3,7 +3,7 @@ $TRANSLATIONS = array(
 "Chat" => "Discussion",
 "{displayname} attached {path} to this conversation" => "{displayname} a attaché {path} à la conversation",
 "{displayname} removed {path} from this conversation" => "{displayname} a supprimé {path} de la conversation",
-"Start a chat with ..." => "Démarrer une conversation avec...",
+"Start a chat with ..." => "Démarrer une conversation avec…",
 "Search in conversations" => "Rechercher dans les conversations",
 "Add Person" => "Ajouter une personne",
 "View Attached files" => "Voir les fichiers attachés",
@@ -11,12 +11,12 @@ $TRANSLATIONS = array(
 "Files attached to this conversation" => "Fichiers ajoutés à la conversation",
 "Download " => "Télécharger",
 "Attach more files" => "Ajouter d'autres fichiers",
-"Search in users" => "Recherche dans les utilisateurs",
-"There are no Chat Backends enabled." => "Aucun système de messagerie instantanée actif",
-"In order to chat please enable at least one Chat backend." => "Pour discuter, veuillez activer au moins un système de messagerie instantanée",
+"Search in users" => "Recherche parmi les utilisateurs",
+"There are no Chat Backends enabled." => "Aucun système de messagerie instantanée actif.",
+"In order to chat please enable at least one Chat backend." => "Pour discuter, veuillez activer au moins un système de messagerie instantanée.",
 "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "Avec le système de messagerie instantanée 'OCH', vous pouvez discuter avec les autres utilisateurs ownCloud. Cela fonctionne sans configuration.",
 "It can be enabled by running the following command in the root of your ownCloud installation:" => "Il peut être activé en lançant la commande suivante dans le dossier racine d'installation de ownCloud :",
 "There are no other users on this ownCloud." => "Aucun autre utilisateur n'est présent sur cet ownCloud.",
-"In order to chat please create at least one user, it will appear on the left." => "Pour pouvoir lancer une discussion, veuillez créer au moins un utilisateur. Il aparaîtra sur la gauche."
+"In order to chat please create at least one user, it will appear on the left." => "Pour pouvoir lancer une discussion, veuillez créer au moins un utilisateur. Il apparaîtra sur la gauche."
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";

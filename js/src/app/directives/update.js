047e049c6281037604cbd12ab371b2c6ee8ca6ed
LEDfan <tobia@ledfan.be>
2015-02-14 12:42:29 +0100

    Add preventdefault to remove enters

--- js/src/app/directives/update.js
+++ js/src/app/directives/update.js
@@ -2,7 +2,8 @@ angular.module('chat').directive('update', function () {
 	return function (scope, element, attrs) {
 		element.bind("keydown keypress", function (event) {
 			if(event.which === 13) {
-				var val = element.val();
+				event.preventDefault();
+				var val = element.val()
 				element.val('');
 				scope.sendChatMsg(val);
 			}

de534e2da76cb83536ccd6d648506c8a86427d91
LEDfan <tobia@ledfan.be>
2015-02-22 08:06:35 +0100

    $c is private now. Fixs #206

--- appinfo/register_command.php
+++ appinfo/register_command.php
@@ -1,5 +1,5 @@
 <?php
 
 $app = new OCA\Chat\App\Chat;
-$application->add(new OCA\Chat\Command\EnableBackend($app->c['BackendManager']));
-$application->add(new OCA\Chat\Command\DisableBackend($app->c['BackendManager']));
+$application->add(new OCA\Chat\Command\EnableBackend($app->query('BackendManager')));
+$application->add(new OCA\Chat\Command\DisableBackend($app->query('BackendManager')));

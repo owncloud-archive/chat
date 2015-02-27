1c0609027e5239f1acd173aba8afe2c4d7835388
LEDfan <tobia@ledfan.be>
2015-02-27 09:04:42 +0100

    Add error page

--- templates/error.php
+++ templates/error.php
@@ -1,4 +1,12 @@
-<p>
-	An error occured in the Chat app.<br />
-	<?php p($_['error']);?>
-</p>
+<section id="error">
+	<h1>An error occurred in the Chat app.</h1><br />
+	<p>
+		<?php p($_['error']);?>
+	</p>
+	<p>
+		<?php p($_['info']);?>
+	</p>
+	<h2><a href="<?php echo $_['link'];?>">More information</a></h2>
+	<h2><a href="https://github.com/owncloud/chat/issues/new">Contact a developer</a></h2>
+
+</section>

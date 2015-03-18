753dc0077563a6c9a88860068d16e6e72bee457b
LEDfan <tobia@ledfan.be>
2015-03-18 17:57:39 +0100

    use closure for app menu + do install things in install.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -12,22 +12,17 @@ use \OCP\Util;
 use \OCP\App;
 
 $chat = new Chat();
-$chat->query('OCP\INavigationManager')->add(array(
-	'id' => 'chat',
-	'order' => 10,
-	'href' => Util::linkToRoute('chat.app.index'),
-	'icon' => Util::imagePath('chat', 'chat.png'),
-	'name' => Util::getL10n('chat')->t('Chat')
-));
+$chat->query('OCP\INavigationManager')->add(function(){
+	return array(
+		'id' => 'chat',
+		'order' => 10,
+		'href' => Util::linkToRoute('chat.app.index'),
+		'icon' => Util::imagePath('chat', 'chat.png'),
+		'name' => Util::getL10n('chat')->t('Chat')
+	);
+});
 $chat->registerBackend($chat->query('OCH'));
 $chat->registerBackend($chat->query('XMPP'));
-
-// Disable the XMPP backend by default when there is no entry in the DB which enables it
-// you can manually enable it (https://github.com/owncloud/chat/wiki/FAQ#enabling-a-backend)
-$enabled =  $chat->query('OCP\IConfig')->getAppValue('chat', 'backend_xmpp_enabled');
-if ($enabled === null){
-	$chat->query('OCP\IConfig')->setAppValue('chat', 'backend_xmpp_enabled', false);
-}
 
 App::registerAdmin('chat', 'lib/admin');
 

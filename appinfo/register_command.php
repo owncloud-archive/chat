51099b3c6dff8d062fc9a78eb3ea472fa39461b0
LEDfan <tobia@ledfan.be>
2015-03-20 13:22:45 +0100

    Update

--- appinfo/register_command.php
+++ appinfo/register_command.php
@@ -1,6 +1,6 @@
 <?php
 
-$app = new OCA\Chat\App\Chat;
-$application->add(new OCA\Chat\Command\EnableBackend($app->query('BackendManager')));
-$application->add(new OCA\Chat\Command\DisableBackend($app->query('BackendManager')));
-$application->add(new OCA\Chat\Command\UnInstall($app->query('OCP\App\IAppManager'), $app->query('OCP\IDb')));
+$container = new OCA\Chat\App\Container();
+$application->add(new OCA\Chat\Command\EnableBackend($container->query('BackendManager')));
+$application->add(new OCA\Chat\Command\DisableBackend($container->query('BackendManager')));
+$application->add(new OCA\Chat\Command\UnInstall($container->query('OCP\App\IAppManager'), $container->query('OCP\IDb')));

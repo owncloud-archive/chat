e042d62145e6c7f0c7a615f03b1d22c4bbffee4e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-04-18 01:11:20 -0400

    [tx-robot] updated from transifex

--- l10n/ro.js
+++ l10n/ro.js
@@ -13,6 +13,10 @@ OC.L10N.register(
     "Download " : "Descarca",
     "Attach more files" : "Ataseaza mai multe fisiere",
     "Search in users" : "Cautare utilizator",
+    "There are no Chat Backends enabled." : "Nu există Chat Backend activat.",
+    "In order to chat please enable at least one Chat backend." : "Pentru a comunica in chat te rog activează cel puțin un Chat backend.",
+    "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." : "Cu 'OCH' backend poți sa comunici in chat cu alți utilizatori ownCloud. Funcționează fără configurare.",
+    "It can be enabled by running the following command in the root of your ownCloud installation:" : "Poate fi activată prin executarea următoarei comenzi în rădăcina instalării ownClowd:",
     "There are no other users on this ownCloud." : "Nu exista alti utilizatori in acest ownCloud",
     "In order to chat please create at least one user, it will appear on the left." : "Pentru a comunica, va rugam creati cel putin un utilizator, acesta va fi afisat in stanga"
 },

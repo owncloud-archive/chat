bf5e901fe2f918acb78c2eadab272cc1bafc28ee
LEDfan <tobia@ledfan.be>
2015-04-18 14:53:30 +0200

    Enable app before running tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,3 +11,5 @@ require_once __DIR__ . '/../../../lib/base.php';
 if(!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');
 }
+
+\OC::$server->getAppManager()->enableApp('chat');

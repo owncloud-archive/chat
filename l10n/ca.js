47c2e79865eb15dda72269756281d68df8818d2c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-04-23 01:11:25 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ca.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "chat",
+    {
+    "Chat" : "Xat",
+    "{displayname} attached {path} to this conversation" : "{displayname} va adjuntar {path} a aquesta conversa",
+    "{displayname} removed {path} from this conversation" : "{displayname} va eliminar {path} d'aquesta conversa",
+    "Start a chat with ..." : "Xateja amb ...",
+    "Search in conversations" : "Cerca en les converses",
+    "Add Person" : "Afegeix Persona",
+    "View Attached files" : "Obre arxius adjunts",
+    "Chat Message" : "Missatge de xat",
+    "Files attached to this conversation" : "Arxius adjuntats a la conversa",
+    "Download " : "Baixada",
+    "Attach more files" : "Adjunta més arxius",
+    "Search in users" : "Cerca en els usuaris",
+    "There are no Chat Backends enabled." : "No hi ha habilitats \"Xat Backends\".",
+    "In order to chat please enable at least one Chat backend." : "Per poder xatejar ha d'habilitar almenys un \"Xat Backend\".",
+    "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." : "Pot xatejar amb altres usuaris de ownCloud amb el \"OCH backend\". No requereix configuració.",
+    "It can be enabled by running the following command in the root of your ownCloud installation:" : "Es pot habilitar executant el següent comando en l'arrel de la instal·lació ownCloud:",
+    "There are no other users on this ownCloud." : "No hi ha altres usuaris en aquest ownCloud",
+    "In order to chat please create at least one user, it will appear on the left." : "Per xatejar siusplau crea almenys un usuari, apareixerà a l'esquerra"
+},
+"nplurals=2; plural=(n != 1);");

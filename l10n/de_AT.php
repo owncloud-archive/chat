662880a222db469bd0a533612ac4e645157edfa8
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-05-11 01:11:24 -0400

    [tx-robot] updated from transifex

---
+++ l10n/de_AT.php
@@ -0,0 +1,22 @@
+<?php
+$TRANSLATIONS = array(
+"Chat" => "Chat",
+"{displayname} attached {path} to this conversation" => "{displayname} fügte {path} zu dieser Konversation hinzu",
+"{displayname} removed {path} from this conversation" => "{displayname} entfernte {path} von dieser Konversation",
+"Start a chat with ..." => "Starte einen Chat mit ...",
+"Search in conversations" => "In Konversationen suchen",
+"Add Person" => "Person hinzufügen",
+"View Attached files" => "Angehängte Dateien anzeigen",
+"Chat Message" => "Chat Nachricht",
+"Files attached to this conversation" => "Angehängte Dateien in dieser Konversation",
+"Download " => "Herunterladen",
+"Attach more files" => "Mehr Dateien anhängen",
+"Search in users" => "In Benutzern suchen",
+"There are no Chat Backends enabled." => "Es sind keine Chat Backends eingeschaltet",
+"In order to chat please enable at least one Chat backend." => "Um zu chatten musst du mindestens ein Chat Backend einschalten.",
+"With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "Mit dem \"OCH\" Backend kannst du mit anderen OwnCloud usern chatten. Funktioniert ohne Konfiguration.",
+"It can be enabled by running the following command in the root of your ownCloud installation:" => "Es kann eingeschaltet werden indem du folgendes Kommando im Stammverzeichnis ihrer ownCloud Installation eingeben:",
+"There are no other users on this ownCloud." => "Es gibt keine anderen Benutzer auf dieser ownCloud.",
+"In order to chat please create at least one user, it will appear on the left." => "Erstelle mindestens einen Benutzer um zu chatten. Er wird auf der linken Seite auftauchen."
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

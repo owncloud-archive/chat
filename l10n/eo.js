07809c7ff5839dc92e0f0e8bbbbcafd9586de10c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-02 01:11:19 -0400

    [tx-robot] updated from transifex

---
+++ l10n/eo.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "chat",
+    {
+    "Chat" : "Babilo",
+    "{displayname} attached {path} to this conversation" : "{displayname} kunligis {path} al ĉi tiu babilo",
+    "{displayname} removed {path} from this conversation" : "{displayname} forigis {path} el ĉi tiu babilo",
+    "Start a chat with ..." : "Komenci babilon kun...",
+    "Search in conversations" : "Serĉi inter babiloj",
+    "Add Person" : "Aldoni ulon",
+    "View Attached files" : "Vidi kunligitajn dosierojn",
+    "Chat Message" : "Babilmesaĝo",
+    "Files attached to this conversation" : "Dosieroj kunligitaj kun ĉi tiu babilo",
+    "Download " : "Elŝuti",
+    "Attach more files" : "Kunligi pli da dosieroj",
+    "Search in users" : "Serĉi inter uzantoj",
+    "There are no other users on this ownCloud." : "Estas neniu alia uzanto en ĉi tiu ownCloud.",
+    "In order to chat please create at least one user, it will appear on the left." : "Por babili, bonvolu krei almenaŭ unu uzanton; ĝi aperos maldekstre."
+},
+"nplurals=2; plural=(n != 1);");

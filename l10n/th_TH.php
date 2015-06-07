340561bce494151eff870b6928412c6f7886e302
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-07 01:11:18 -0400

    [tx-robot] updated from transifex

---
+++ l10n/th_TH.php
@@ -0,0 +1,22 @@
+<?php
+$TRANSLATIONS = array(
+"Chat" => "สนทนา",
+"{displayname} attached {path} to this conversation" => "ในการสนทนานี้ {displayname} แนบ {path} มาด้วย ",
+"{displayname} removed {path} from this conversation" => "{displayname} ลบ {path} ออกจากการสนทนานี้",
+"Start a chat with ..." => "เริ่มการสนทนากับ ...",
+"Search in conversations" => "ค้นหาในการสนทนา",
+"Add Person" => "เพิ่มบุคคล",
+"View Attached files" => "ดูไฟล์ที่แนบมา",
+"Chat Message" => "ข้อความสนทนา",
+"Files attached to this conversation" => "ไฟล์ที่แนบมากับการสนทนานี้",
+"Download " => "ดาวน์โหลด",
+"Attach more files" => "แนบไฟล์อื่นๆ",
+"Search in users" => "ค้นหาผู้ใช้",
+"There are no Chat Backends enabled." => "ไม่มีบทสนทนาที่เปิดใช้งานอยู่",
+"In order to chat please enable at least one Chat backend." => "เพื่อที่จะสนทนาโปรดเปิดใช้อย่างน้อยหนึ่งการสนทนา",
+"With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "ด้วยส่วนหลัง 'OCH' คุณสามารถสนทนากับผู้ใช้อื่นๆ มันทำงานได้โดยไม่ต้องตั้งค่า",
+"It can be enabled by running the following command in the root of your ownCloud installation:" => "มันสามารถเปิดใช้งานโดยการเรียกใช้คำสั่งต่อไปในรากของการติดตั้ง ownCloud คุณ:",
+"There are no other users on this ownCloud." => "ไม่มีผู้ใช้อื่นใน ownCloud นี้",
+"In order to chat please create at least one user, it will appear on the left." => "เพื่อที่จะสนทนากรุณาสร้างผู้ใช้อย่างน้อยหนึ่งคนมันจะปรากฏด้านซ้าย"
+);
+$PLURAL_FORMS = "nplurals=1; plural=0;";

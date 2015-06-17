7c27841bcf61d7bc24e649966e57245a6a21f35d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-17 01:11:24 -0400

    [tx-robot] updated from transifex

---
+++ l10n/oc.php
@@ -0,0 +1,22 @@
+<?php
+$TRANSLATIONS = array(
+"Chat" => "Discussion",
+"{displayname} attached {path} to this conversation" => "{displayname} a estacat {path} a la conversacion",
+"{displayname} removed {path} from this conversation" => "{displayname} a suprimit {path} de la conversacion",
+"Start a chat with ..." => "Aviar una conversacion amb…",
+"Search in conversations" => "Recercar dins las conversacions",
+"Add Person" => "Apondre una persona",
+"View Attached files" => "Veire los fichièrs estacats",
+"Chat Message" => "Messatge de discussion",
+"Files attached to this conversation" => "Fichièrs aponduts a la conversacion",
+"Download " => "Telecargar",
+"Attach more files" => "Apondre d'autres fichièrs",
+"Search in users" => "Recèrca demest los utilizaires",
+"There are no Chat Backends enabled." => "Pas cap de sistèma de messatjariá instantanèa d'actiu.",
+"In order to chat please enable at least one Chat backend." => "Per discutir, activatz al mens un sistèma de messatjariá instantanèa.",
+"With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "Amb lo sistèma de messatjariá instantanèa 'OCH', podètz discutir amb los autres utilizaires ownCloud. Aquò fonciona sens configuracion.",
+"It can be enabled by running the following command in the root of your ownCloud installation:" => "Pòt èsser activat en aviant la comanda seguenta dins lo dorsièr raiç d'installacion de ownCloud :",
+"There are no other users on this ownCloud." => "Cap d'autre utilizaire es pas present sus aqueste ownCloud.",
+"In order to chat please create at least one user, it will appear on the left." => "Per poder aviar una discussion, creatz al mens un utilizaire. Apareisserà a esquèrra."
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n > 1);";

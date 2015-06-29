816624e5700d8fba76e55d0d942cd967b0a4a63f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-29 01:11:13 -0400

    [tx-robot] updated from transifex

--- l10n/et_EE.php
+++ l10n/et_EE.php
@@ -1,8 +1,9 @@
 <?php
 $TRANSLATIONS = array(
 "Chat" => "Vestlus",
-"{displayname} attached {path} to this conversation" => "{displayname} lisas sellele vestlsele {path}",
-"{displayname} removed {path} from this conversation" => "{displayname} eemaldas sellest vestlsest {path}",
+"{displayname} attached {path} to this conversation" => "{displayname} lisas sellele vestlusele {path}",
+"{displayname} removed {path} from this conversation" => "{displayname} eemaldas sellest vestlusest {path}",
+"Start a chat with ..." => "Alusta vestlust kasutajaga ...",
 "Search in conversations" => "Otsi vestlustest",
 "Add Person" => "Lisa inimene",
 "View Attached files" => "Vaata lisatud faile",
@@ -14,6 +15,7 @@ $TRANSLATIONS = array(
 "There are no Chat Backends enabled." => "Ühtegi vestluse tagarakendit pole lubatud.",
 "In order to chat please enable at least one Chat backend." => "Vestluste kasutamiseks palun luba vähemalt üks vestluste tagarakend.",
 "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "'OCH' tagarakendiga saad vestelda teiste ownCloud kasutajatega. See ei nõua eraldi seadistamist.",
+"It can be enabled by running the following command in the root of your ownCloud installation:" => "Seda saab sisse lülitada käivitades sinu ownCloudi juurkaustast järgneva käsu:",
 "There are no other users on this ownCloud." => "Selles ownCloudis pole ühtegi teist kasutajat.",
 "In order to chat please create at least one user, it will appear on the left." => "Vestlemiseks loo palun vähemalt üks kasutaja. See on nähtav vasakul."
 );

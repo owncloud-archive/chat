57179375240bb698a8017c6aaafdeaa7ba71363a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-03 01:11:31 -0400

    [tx-robot] updated from transifex

--- l10n/fa.php
+++ l10n/fa.php
@@ -1,10 +1,18 @@
 <?php
 $TRANSLATIONS = array(
 "Chat" => "گفتگوی متنی",
+"{displayname} attached {path} to this conversation" => "{displayname} ضمیمه‌ شده {path} به این گفتگو",
+"{displayname} removed {path} from this conversation" => "{displayname} حذف شده {path} از این گفتگو",
+"Start a chat with ..." => "آغاز گفتگو با ...",
 "Search in conversations" => "جستجو در مکالمات",
 "Add Person" => "افزودن فرد",
+"View Attached files" => "نمایش فایل‌های ضمیمه شده",
 "Chat Message" => "پیغام چت",
+"Files attached to this conversation" => "فایل‌های ضمیمه شده به این گفتگو",
+"Download " => "دانلود",
+"Attach more files" => "ضمیمه کردن فایل‌های بیشتر",
+"Search in users" => "جستجو در کاربران",
 "There are no other users on this ownCloud." => "کاربر دیگری در این سرور owncloud وجود ندارد.",
-"\tIn order to chat please create at least one user, it will appear on the left." => "برای شروع گفتگو، لطفا حداقل یک کاربر ایجاد کنید. که در بخش سمت چپ قابل مشاهده است."
+"In order to chat please create at least one user, it will appear on the left." => "لطفا حداقل یک کاربر به منظور گفتگو ایجاد کنید، در سمت چپ قابل مشاهده خواهد بود."
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";

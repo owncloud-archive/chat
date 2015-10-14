1cfc7f74dc6b49e4dc0e56d65d365aee02e735ed
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-14 01:11:29 -0400

    [tx-robot] updated from transifex

---
+++ l10n/sq.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "chat",
+    {
+    "Chat" : "Fjalosje",
+    "{displayname} attached {path} to this conversation" : "{displayname} i bashkëngjiti {path} kësaj bisede",
+    "{displayname} removed {path} from this conversation" : "{displayname} hoqi {path} nga kjo bisedë",
+    "Start a chat with ..." : "Filloni në fjalosje me ...",
+    "Search in conversations" : "Kërkoni te bisedat",
+    "Add Person" : "Shtoni Person",
+    "View Attached files" : "Shihni kartela e Bashkëngjitura",
+    "Chat Message" : "Mesazh Fjalosjeje",
+    "Files attached to this conversation" : "Kartela bashkëngjitur kësaj bisede",
+    "Download " : "Shkarkojeni",
+    "Attach more files" : "Bashkëngjitni më tepër kartela",
+    "Search in users" : "Kërkoni te përdoruesit",
+    "There are no Chat Backends enabled." : "Nuk ka të aktivizuar Program Shërbyesi Fjalosjesh.",
+    "In order to chat please enable at least one Chat backend." : "Që të mund të fjaloseni, ju lutemi aktivizoni të paktën një program shërbyesi fjalosjesh.",
+    "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." : "Me programin 'OCH' mund të fjaloseni me përdorues të tjerë të ownCloud-it. Funksionon pa formësim.",
+    "It can be enabled by running the following command in the root of your ownCloud installation:" : "Mund të aktivizohet duke xhiruar urdhrin vijues në rrënjë të instalimit tuaj të ownCloud-it:",
+    "There are no other users on this ownCloud." : "S’ka përdorues të tjerë në këtë ownCloud.",
+    "In order to chat please create at least one user, it will appear on the left." : "Që të mund të fjaloseni, ju lutemi, krijoni të paktën një përdorues, ky do të duket majtas."
+},
+"nplurals=2; plural=(n != 1);");

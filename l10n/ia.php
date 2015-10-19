5e5808209228bd7ee2c3df54fec1378cd776ff57
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-19 01:11:31 -0400

    [tx-robot] updated from transifex

--- l10n/ia.php
+++ l10n/ia.php
@@ -1,10 +1,21 @@
 <?php
 $TRANSLATIONS = array(
 "Chat" => "Conversation in directo",
+"{displayname} attached {path} to this conversation" => "{displayname} ha attachate {path} a iste conversation",
+"{displayname} removed {path} from this conversation" => "{displayname} ha removite {path} de iste conversation",
+"Start a chat with ..." => "Initiar un conversation con ...",
 "Search in conversations" => "Cerca in conversationes",
 "Add Person" => "Adde persona",
+"View Attached files" => "Vider files attachate",
 "Chat Message" => "Message de conversation",
+"Files attached to this conversation" => "Files attachate a iste conversation",
+"Download " => "Discargar",
+"Attach more files" => "Attachar altere files",
 "Search in users" => "Cerca in usatores",
+"There are no Chat Backends enabled." => "Nulle mechanismo de chat ha essite activate.",
+"In order to chat please enable at least one Chat backend." => "Pro conversar, per favor activa al minus un mechanismo de chat.",
+"With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "Con le mechanismo 'OCH' tu pote chattar con altere usatores de ownCloud. Isto functiona sin configuration.",
+"It can be enabled by running the following command in the root of your ownCloud installation:" => "Activa lo per le execution del sequente commando in le radice de tu installation ownCloud:",
 "There are no other users on this ownCloud." => "Il non ha alcun altere usatores in iste ownCloud",
 "In order to chat please create at least one user, it will appear on the left." => "Pro poter conversar pro favor tu crea al minus un usator, illo apparera a le sinistra."
 );

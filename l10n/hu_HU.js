f0e2f4d5402567be10df021aea30b70739ae99db
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-12-29 01:11:18 -0500

    [tx-robot] updated from transifex

---
+++ l10n/hu_HU.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "chat",
+    {
+    "Chat" : "Csevegés",
+    "{displayname} attached {path} to this conversation" : "{displayname} csatolva {path} ehhez a beszélgetéshez",
+    "{displayname} removed {path} from this conversation" : "{displayname} eltávolítva {path} ebből a beszélgetésből",
+    "Start a chat with ..." : "Csevegés megkezdése vele ...",
+    "Search in conversations" : "Keresés a beszélgetésekben",
+    "Add Person" : "Személy Hozzáadása",
+    "View Attached files" : "Csatolt fájlok megtekintése",
+    "Chat Message" : "Csevegö Üzenet",
+    "Files attached to this conversation" : "Ehhez a beszélgetéshez csatolt fájlok.",
+    "Download " : "Letöltés",
+    "Attach more files" : "Több fájl csatolása",
+    "Search in users" : "Keresés a felhasználók között",
+    "There are no Chat Backends enabled." : "Nincs csevegés háttérkiszolgáló engedélyezve.",
+    "In order to chat please enable at least one Chat backend." : "A csevegés megindításához kérem engedélyezzen legalább egy háttérkiszolgálót.",
+    "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." : "Az 'OCH' háttérkiszolgáló segítégével cseveghet másik ownCloud felhasználókkal. Konfiguráció nélkül működik.",
+    "It can be enabled by running the following command in the root of your ownCloud installation:" : "Az ownCloud gyökérkönyvtárában a következő parancs kiadásával engedélyezhező:",
+    "There are no other users on this ownCloud." : "Nincs más felhasználó ebben az ownCloud-ban.",
+    "In order to chat please create at least one user, it will appear on the left." : "A csevegés megkezdéséhez kérem hozzon létre legalább egy felhasználót, ez a bal oldalon fog látszani."
+},
+"nplurals=2; plural=(n != 1);");

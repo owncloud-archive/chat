827fb41b8c66d1f465d47ab005eb46a5c473efe2
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-09 01:11:26 -0500

    [tx-robot] updated from transifex

--- l10n/he.php
+++ l10n/he.php
@@ -1,10 +1,21 @@
 <?php
 $TRANSLATIONS = array(
 "Chat" => "שיחה",
+"{displayname} attached {path} to this conversation" => "המשתמש {displayname} צירף {path} לשיחה זו",
+"{displayname} removed {path} from this conversation" => "המשתמש {displayname} הסיר {path} משיחה זו",
+"Start a chat with ..." => "התחלת שיחה עם...",
 "Search in conversations" => "חיפוש בשיחות",
 "Add Person" => "הוספת חבר",
+"View Attached files" => "הצגת קבצים מצורפים",
 "Chat Message" => "הודעת שיחה",
+"Files attached to this conversation" => "קיימים קבצים מצורפים לשיחה זו",
+"Download " => "הורדה",
+"Attach more files" => "צירוף קבצים נוספים",
 "Search in users" => "חיפוש במשתמשים",
+"There are no Chat Backends enabled." => "הצד האחורי של ה- Chat מנוטרל.",
+"In order to chat please enable at least one Chat backend." => "על מנת לאפשר שיחה יש לאפשר לפחות צד אחורי אחד ל- Chat.",
+"With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "על בסיס צד אחורי של 'OCH' ניתן לשוחח עם משתמשי ownCloud אחרים. אפשרות זו עובדת ללא הגדרות.",
+"It can be enabled by running the following command in the root of your ownCloud installation:" => "ניתן לאפשר זאת על ידי הרצת הפקודה הבאה בנתיב היסודי של התקנת ה- ownCloud:",
 "There are no other users on this ownCloud." => "אין משתמשים נוספים ב- ownCloud הזה",
 "In order to chat please create at least one user, it will appear on the left." => "כדי לבצע שיחה, יש ליצור לפחות משתמש אחד שיופיע בצד שמאל"
 );

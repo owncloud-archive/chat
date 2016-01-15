031414d39caaedc0f39d21a66106804d024eef73
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-15 01:11:24 -0500

    [tx-robot] updated from transifex

--- l10n/zh_TW.js
+++ l10n/zh_TW.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Chat" : "聊天",
     "{displayname} attached {path} to this conversation" : "{displayname} 已附加 {path} 到這個談話",
     "{displayname} removed {path} from this conversation" : "{displayname} 已移除 {path} 從這個談話",
+    "Start a chat with ..." : "開始聊天 ...",
     "Search in conversations" : "在談話中搜尋",
     "Add Person" : "新增個人",
     "View Attached files" : "檢視已附加檔案",
@@ -15,6 +16,7 @@ OC.L10N.register(
     "There are no Chat Backends enabled." : "沒有啟用背景聊天。",
     "In order to chat please enable at least one Chat backend." : "請至少開啟一個背景聊天來聊天。",
     "With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." : "在 OwnCloud 背景聊天中您可以跟其他 ownCloud 使用者聊天，這個動作是不需要設定的。",
+    "It can be enabled by running the following command in the root of your ownCloud installation:" : "在 ownCloud 安装資料夾底下用 root 執行下面指令可以啟用:",
     "There are no other users on this ownCloud." : "沒有其他使用者在這個 ownCloud。",
     "In order to chat please create at least one user, it will appear on the left." : "請建立至少一個使用者來進行聊天，他將會出現在左邊。"
 },

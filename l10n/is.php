ef0c1c36cdaf19c955be38c4c77f1d5c808fcd2a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-24 15:12:18 -0500

    [tx-robot] updated from transifex

--- l10n/is.php
+++ l10n/is.php
@@ -2,21 +2,21 @@
 $TRANSLATIONS = array(
 "Chat" => "Spjall",
 "{displayname} attached {path} to this conversation" => "{displayname} festi {path} við þetta samtal",
-"{displayname} removed {path} from this conversation" => "{displayname} fjarlægð {path} úr þessu samtali",
+"{displayname} removed {path} from this conversation" => "{displayname} fjarlægði {path} úr þessu samtali",
 "Start a chat with ..." => "Byrja spjall við ...",
 "Search in conversations" => "Leita í samtölum",
 "Add Person" => "Bæta við aðila",
 "View Attached files" => "Skoða viðhengi",
-"Chat Message" => "Spjall skilaboð",
-"Files attached to this conversation" => "Skrár sem viðhengi í þessu samtal",
+"Chat Message" => "Spjallskilaboð",
+"Files attached to this conversation" => "Skrár sem viðhengd eru í þessu samtal",
 "Download " => "Niðurhal",
-"Attach more files" => "Hengja fleiri skrár",
+"Attach more files" => "Hengja við fleiri skrár",
 "Search in users" => "Leita í notendum",
-"There are no Chat Backends enabled." => "Engir Spjallrása bakendar virkir.",
-"In order to chat please enable at least one Chat backend." => "Til að spjalla vinsamlegast virkið að minnsta kosti einn spjall bakenda.",
-"With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "Með 'och' stuðningur þú getur spjallað við aðra ownCloud notendur. Það virkar án frekari stillinga.",
-"It can be enabled by running the following command in the root of your ownCloud installation:" => "Það er hægt að virkja með því að keyra eftirfarandi skipun sem rót á ownCloud uppsetningunni þinni:",
-"There are no other users on this ownCloud." => "Engir aðrir notendur á þessum ownCloud.",
-"In order to chat please create at least one user, it will appear on the left." => "Til að geta spjallað vinsamlegast búa til að minnsta kosti einn notanda, hann mun birtast til vinstri."
+"There are no Chat Backends enabled." => "Engir spjallrásabakendar virkir.",
+"In order to chat please enable at least one Chat backend." => "Til að spjalla, virkjaðu að minnsta kosti einn spjallbakenda.",
+"With the 'OCH' backend you can chat with other ownCloud users. It works without configuration." => "Með 'OHC'-bakenda þú getur spjallað við aðra ownCloud notendur. Það virkar án frekari stillinga.",
+"It can be enabled by running the following command in the root of your ownCloud installation:" => "Það er hægt að virkja með því að keyra eftirfarandi skipun sem rót/kerfisstjórnandi á ownCloud uppsetningunni þinni:",
+"There are no other users on this ownCloud." => "Það eru engir aðrir notendur á þessum ownCloud.",
+"In order to chat please create at least one user, it will appear on the left." => "Til að geta spjallað þarftu að búa til að minnsta kosti einn notanda, hann mun birtast til vinstri."
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);";
